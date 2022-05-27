a318db21f0aa3113f6a86608fed18deaf3d42bf3
FenrisWolf <albertomg_94@hotmail.com>
2022-05-27 15:03:09 -0500

    Se agrego el guard en el dashboard que checa si el usuario esta logeado o no

--- layouts/MasterPage/phpcode/_dashboardphp.php
+++ layouts/MasterPage/phpcode/_dashboardphp.php
@@ -1,3 +1,14 @@
 <?php
-
+    include_once("../config/conexion.php");
+    $object = new connection_database();
+    
+    session_start();
+    if (isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
+        header('Location: Login.php');
+        die();
+    }
+    
+    $perfil = $object->_db->prepare("SELECT * FROM usuarios WHERE id=:sessionid");
+    $perfil->bindParam("sessionid", $_SESSION['id'], PDO::PARAM_INT);
+    $perfil->execute();
 ?>
