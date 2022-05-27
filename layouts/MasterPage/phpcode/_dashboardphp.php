2a3c57176e3b53735eba7caa92136f5e15da70e5
FenrisWolf <albertomg_94@hotmail.com>
2022-05-27 15:26:41 -0500

    Se empezo a agregar datos reales a la intranet

--- layouts/MasterPage/phpcode/_dashboardphp.php
+++ layouts/MasterPage/phpcode/_dashboardphp.php
@@ -8,7 +8,8 @@
         die();
     }
     
-    $perfil = $object->_db->prepare("SELECT * FROM usuarios WHERE id=:sessionid");
+    $perfil = $object->_db->prepare("SELECT usuarios.nombre as nombre, usuarios.apellido_pat as apellido_pat, usuarios.apellido_mat as apellido_mat, roles.nombre as rolnom FROM usuarios inner join roles on usuarios.roles_id=roles.id WHERE usuarios.id=:sessionid");
     $perfil->bindParam("sessionid", $_SESSION['id'], PDO::PARAM_INT);
     $perfil->execute();
+    $profile = $perfil -> fetch(PDO::FETCH_OBJ);
 ?>
