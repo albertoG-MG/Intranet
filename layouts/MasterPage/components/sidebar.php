b96c5ece5a3be7b35eec0b1c80563748ffad5ea2
FenrisWolf <albertomg_94@hotmail.com>
2022-05-27 16:45:58 -0500

    Se arreglo el hover del boton

--- layouts/MasterPage/components/sidebar.php
+++ layouts/MasterPage/components/sidebar.php
@@ -18,7 +18,7 @@
         <span class="mx-3">Dashboard</span>
     </a>
 
-    <button class="flex w-full items-center mt-4 py-2 px-6 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100" data-collapse-toggle="catalogos">
+    <button class="flex w-full items-center mt-4 py-2 px-6 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 group hover:text-gray-100" data-collapse-toggle="catalogos">
         <svg class="flex-shrink-0 w-6 h-6 text-gray-400 transition duration-75 group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z">
             </path>
