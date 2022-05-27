fbf542b46857b1d5ca367ee25dfeffefd26c6f5e
FenrisWolf <albertomg_94@hotmail.com>
2022-05-27 10:19:15 -0500

    Se agrego flowbite al bundle

--- webpack/app.jsx
+++ webpack/app.jsx
@@ -3,5 +3,6 @@ import 'jquery-validation'
 import 'jquery-ui-sortable'
 import sweetalert2 from 'sweetalert2'
 import formBuilder from 'formBuilder'
+import 'flowbite'
 window.Swal = sweetalert2
 window.formBuilder = formBuilder
