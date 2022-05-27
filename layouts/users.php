4a4c70f9b308ec6558b38ff98cad227e1ae01d55
FenrisWolf <albertomg_94@hotmail.com>
2022-05-27 17:05:36 -0500

    Se agrego la vista de los usuarios

---
+++ layouts/users.php
@@ -0,0 +1,9 @@
+<?php
+$title = 'Vista usuarios';
+$sidebar = 'components/sidebar.php';
+$navbar = 'components/navbar.php';
+$content = 'content/_users.php';
+$codigophp = 'phpcode/_usersphp.php';
+$scripts = 'scripts/_usersscript.php';
+include('MasterPage/masterpage.php');
+?>
