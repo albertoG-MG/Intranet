44e2326d062c6e832cd41184b7b82e6b8d65a1ba
FenrisWolf <albertomg_94@hotmail.com>
2022-05-31 12:21:05 -0500

    Se agrego un icono al input

--- layouts/MasterPage/content/_crear_usuario.php
+++ layouts/MasterPage/content/_crear_usuario.php
@@ -13,8 +13,11 @@
     </div>
 
     <div class="grid grid-cols-1 mt-5 mx-7">
-      <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Input 1</label>
-      <input class="py-2 px-3 rounded-lg border-2 border-indigo-600 mt-1 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:border-transparent" type="text" placeholder="Input 1" />
+      <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Usuario</label>
+      <div class="group flex">
+        <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
+          <input class="w-full -ml-10 pl-10 py-2 px-3 rounded-lg border-2 border-indigo-600 mt-1 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:border-transparent" type="text" placeholder="Input 1">
+        </div>      
     </div>
 
     <div class="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 mt-5 mx-7">
