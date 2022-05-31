5e58d85d12a4fdc70c1007305a10f17b5c51f4fb
FenrisWolf <albertomg_94@hotmail.com>
2022-05-31 14:30:00 -0500

    Se terminaron las 3 columnas

--- layouts/MasterPage/content/_crear_usuario.php
+++ layouts/MasterPage/content/_crear_usuario.php
@@ -39,16 +39,22 @@
               <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Nombre</label>
               <div class="group flex">
                 <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-account-edit-outline text-gray-400 text-lg"></i></div>
-                <input class="w-full -ml-10 pl-10 py-2 px-3 rounded-lg border-2 border-indigo-600 mt-1 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:border-transparent" type="text" placeholder="input 2">
+                <input class="w-full -ml-10 pl-10 py-2 px-3 rounded-lg border-2 border-indigo-600 mt-1 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:border-transparent" type="text" placeholder="Input 2">
               </div>
             </div>
             <div class="grid grid-cols-1">
               <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Apellido paterno</label>
-              <input class="py-2 px-3 rounded-lg border-2 border-indigo-600 mt-1 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:border-transparent" type="text" placeholder="Input 3" />
+              <div class="group flex">
+                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-account-edit-outline text-gray-400 text-lg"></i></div>
+                <input class="w-full -ml-10 pl-10 py-2 px-3 rounded-lg border-2 border-indigo-600 mt-1 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:border-transparent" type="text" placeholder="Input 3">
+              </div>
             </div>
             <div class="grid grid-cols-1">
               <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Apellido materno</label>
-              <input class="py-2 px-3 rounded-lg border-2 border-indigo-600 mt-1 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:border-transparent" type="text" placeholder="Input 4" />
+              <div class="group flex">
+                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-account-edit-outline text-gray-400 text-lg"></i></div>
+                <input class="w-full -ml-10 pl-10 py-2 px-3 rounded-lg border-2 border-indigo-600 mt-1 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:border-transparent" type="text" placeholder="Input 4">
+              </div>
             </div>
           </div>
 
