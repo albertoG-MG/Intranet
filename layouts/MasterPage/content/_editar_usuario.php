194bc9a86198ecea64f3c81c2f537f22ea72ae11
FenrisWolf <albertomg_94@hotmail.com>
2022-06-03 17:18:50 -0500

    Se termino el crud de editar

--- layouts/MasterPage/content/_editar_usuario.php
+++ layouts/MasterPage/content/_editar_usuario.php
@@ -31,7 +31,7 @@
               <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Contraseña</label>
               <div class="group flex">
                 <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
-                <input class="w-full -ml-10 pl-10 py-2 px-3 rounded-lg border-2 border-indigo-600 mt-1 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:border-transparent" type="password" id="password" name="password" placeholder="************">
+                <input class="w-full -ml-10 pl-10 py-2 px-3 rounded-lg border-2 border-indigo-600 mt-1 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:border-transparent" type="password" id="password" name="password" value="<?php echo "{$row->password}"; ?>" placeholder="************">
               </div>
             </div>
 
@@ -39,7 +39,7 @@
               <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Confirmar contraseña</label>
               <div class="group flex">
                 <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
-                <input class="w-full -ml-10 pl-10 py-2 px-3 rounded-lg border-2 border-indigo-600 mt-1 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:border-transparent" type="password" id="cpassword" name="cpassword" placeholder="************">
+                <input class="w-full -ml-10 pl-10 py-2 px-3 rounded-lg border-2 border-indigo-600 mt-1 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:border-transparent" type="password" id="cpassword" name="cpassword" value="<?php echo "{$row->password}"; ?>" placeholder="************">
               </div>
             </div>
 
@@ -93,7 +93,7 @@
                 <label class='flex flex-col border-4 border-dashed w-full h-32 hover:bg-gray-100 hover:border-indigo-800 group'>
                   <div class='flex flex-col items-center justify-center pt-7'>
                     <div id="svg">
-                      <?php if($row->foto == null){ ?>
+                      <?php if($row->foto == null && $row->nombre_foto == null){ ?>
                         <svg class="w-10 h-10 text-indigo-600 group-hover:text-indigo-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
                         </svg>
