d09df0c56067a06b86d6563b5c3068d31b37d83e
FenrisWolf <albertomg_94@hotmail.com>
2022-06-03 12:43:43 -0500

    Ya reedirecciona el icono de editar

--- layouts/MasterPage/scripts/_usersscript.php
+++ layouts/MasterPage/scripts/_usersscript.php
@@ -75,7 +75,7 @@ document.addEventListener("DOMContentLoaded", function() {
                return  (
                    "<div class='py-3 text-left'>"+
                         "<div class='flex item-center justify-center'>"+
-                            "<div class='w-4 mr-2 transform hover:text-purple-500 hover:scale-110'>"+
+                            "<div class='w-4 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer' onclick='Editaruser("+row[0]+"); return false;'>"+
                                 "<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>"+
                                     "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z'></path>"+
                                 "</svg>"+
@@ -97,6 +97,13 @@ $(document).ready(function() {
     $('.dataTables_filter input[type="search"]').
     attr('placeholder', 'Buscar...').attr('class', 'search w-full rounded-lg text-gray-600 font-medium')
 });
+
+function Editaruser(id){
+    var EditarElemento = id;
+    location.href = "editar_usuario.php?idUser=" +EditarElemento;
+}
+
+
 <?php
 if(basename($_SERVER['PHP_SELF']) == 'users.php'){?>
     var dropdown = document.getElementById('catalogos');
