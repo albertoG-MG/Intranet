a203333dc168d632cf6fb2989ed47dec348cd332
FenrisWolf <albertomg_94@hotmail.com>
2022-06-06 14:06:46 -0500

    Se modifico la base de datos

--- database/Intranet.sql
+++ database/Intranet.sql
@@ -11,10 +11,37 @@ CREATE TABLE `roles` (
   `nombre` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
+-- --------------------------------------------------------
+
 -- Insert into para la tabla `roles`
 
 INSERT INTO `roles` (`id`, `nombre`) VALUES
 (1, 'Superadministrador');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `permisos`
+--
+
+CREATE TABLE `permisos` (
+  `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `nombre` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rolesxpermisos`
+--
+
+CREATE TABLE `rolesxpermisos` (
+  `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `roles_id` int NOT NULL,
+  `permisos_id` int NOT NULL,
+  FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE CASCADE,
+  FOREIGN KEY (permisos_id) REFERENCES permisos(id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
 -- --------------------------------------------------------
 
@@ -24,16 +51,16 @@ INSERT INTO `roles` (`id`, `nombre`) VALUES
 
 CREATE TABLE `usuarios` (
   `id` int  NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `username` varchar(30) NOT NULL,
+  `username` varchar(30) NOT NULL UNIQUE,
   `nombre` varchar(100) NOT NULL,
   `apellido_pat` varchar(100) NOT NULL,
   `apellido_mat` varchar(100) NOT NULL,
-  `correo` varchar(100) NOT NULL,
+  `correo` varchar(100) NOT NULL UNIQUE,
   `password` varchar(100) NOT NULL,
   `roles_id` int DEFAULT NULL,
   `nombre_foto` longtext DEFAULT NULL,
   `foto` longtext DEFAULT NULL,
-   FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE CASCADE
+   FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
 -- --------------------------------------------------------
@@ -44,3 +71,5 @@ CREATE TABLE `usuarios` (
 DROP TABLE IF EXISTS `serverside_user`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=CURRENT_USER SQL SECURITY DEFINER VIEW `serverside_user`  AS SELECT `usuarios`.`id` AS `id`, `usuarios`.`username` AS `username`, `usuarios`.`nombre` AS `usnom`, `usuarios`.`apellido_pat` AS `apellido_pat`, `usuarios`.`apellido_mat` AS `apellido_mat`, `usuarios`.`correo` AS `correo`, `usuarios`.`roles_id` AS `roles_id`, `usuarios`.`foto` AS `foto`, `roles`.`nombre` AS `rolnom` FROM (`usuarios` left join `roles` on((`usuarios`.`roles_id` = `usuarios`.`roles_id`)))  ;
+
+-- --------------------------------------------------------
