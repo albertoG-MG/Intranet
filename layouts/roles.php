955d6dfc829f3860a7887a38634894d8c743d757
FenrisWolf <albertomg_94@hotmail.com>
2022-06-06 11:15:23 -0500

    Se agrego el layout de los roles

---
+++ layouts/roles.php
@@ -0,0 +1,9 @@
+<?php
+$title = 'Roles';
+$sidebar = 'components/sidebar.php';
+$navbar = 'components/navbar.php';
+$content = 'content/_roles.php';
+$codigophp = 'phpcode/_rolesphp.php';
+$scripts = 'scripts/_rolesscript.php';
+include('MasterPage/masterpage.php');
+?>
