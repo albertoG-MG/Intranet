7fda3b64bf6bb50c0e415bf4432e62f0289fc864
Fenriswolf <albertomg_94@hotmail.com>
2022-06-06 22:24:31 -0500

    Se arreglo el boton y el link que reedirige hacia los permisos

--- layouts/MasterPage/scripts/_rolesscript.php
+++ layouts/MasterPage/scripts/_rolesscript.php
@@ -21,14 +21,14 @@ document.addEventListener("DOMContentLoaded", function() {
 					}
 				},
                 {
-                    text: "<i class='mdi mdi-lock-outline text-white font-semibold text-lg'></i> Crear Permisos",
+                    text: "<i class='mdi mdi-lock-outline text-white font-semibold text-lg'></i> Ver Permisos",
 					attr: {
 						'id': 'Rol',
                         'style': 'background:rgb(79 70 229 / var(--tw-border-opacity));'
 					},
 					className: 'bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg shadow-xl font-medium text-white',
 					action: function(e, dt, node, config) {
-						window.location.href = "crear_permisos.php";
+						window.location.href = "permisos.php";
 					}
                 }
 				],
