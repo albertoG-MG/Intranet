9e4ba13c0c01fe0100ec13644f6b8aece8d93122
FenrisWolf <albertomg_94@hotmail.com>
2022-06-07 11:47:44 -0500

    Se cambio el id y el name de los permisos

--- layouts/MasterPage/content/_permisos.php
+++ layouts/MasterPage/content/_permisos.php
@@ -35,7 +35,7 @@
                             <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Nombre del permiso</label>
                             <div class="group flex">
                                 <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
-                                <input class="w-full -ml-10 pl-10 py-2 px-3 rounded-lg border-2 border-indigo-600 mt-1 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:border-transparent" type="text" id="usuario" name="usuario" placeholder="Input 1">
+                                <input class="w-full -ml-10 pl-10 py-2 px-3 rounded-lg border-2 border-indigo-600 mt-1 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:border-transparent" type="text" id="permiso" name="permiso" placeholder="Input 1">
                             </div>
                         </div>
                     </div>
