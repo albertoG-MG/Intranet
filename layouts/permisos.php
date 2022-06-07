cd4824526aa1d8b96d4abeafa704f0f7c52ab182
Fenriswolf <albertomg_94@hotmail.com>
2022-06-06 22:17:10 -0500

    Esta vez se agregaron correctamente los archivos

---
+++ layouts/permisos.php
@@ -0,0 +1,9 @@
+<?php
+$title = 'Permisos';
+$sidebar = 'components/sidebar.php';
+$navbar = 'components/navbar.php';
+$content = 'content/_permisos.php';
+$codigophp = 'phpcode/_permisosphp.php';
+$scripts = 'scripts/_permisosscript.php';
+include('MasterPage/masterpage.php');
+?>
