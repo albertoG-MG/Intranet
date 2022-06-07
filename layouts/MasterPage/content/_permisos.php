5ced543318068297d37692835d9719bc82ac383c
FenrisWolf <albertomg_94@hotmail.com>
2022-06-07 11:37:16 -0500

    Se agrego el icono al modal

--- layouts/MasterPage/content/_permisos.php
+++ layouts/MasterPage/content/_permisos.php
@@ -28,7 +28,7 @@
         <div id="modal-container" class="modal-container inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-lx transform transition-all sm:my-8 sm:align-middle sm:max-w-lg w-full">
             <div class="modal-wrapper bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div class="modal-wrapper-flex sm:flex sm:items-start">
-                    <div class="modal-icon mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-indigo-100 sm:mx-0 sm:h-10 sm:w-10"></div>
+                    <div class="modal-icon mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-indigo-100 sm:mx-0 sm:h-10 sm:w-10"><i class="mdi mdi-lock-outline text-black font-semibold text-lg"></i></div>
                     <div class="modal-content text-center mt-3 sm:mt-0 sm:mt-0 sm:ml-4 sm:text-left">
                         <h3 class="text-lg font-medium text-gray-900">Crear permiso</h3>
                         <div class="modal-text mt-2">
