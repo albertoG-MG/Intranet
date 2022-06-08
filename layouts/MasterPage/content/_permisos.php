2389e6d141346a9fc9a274336d4c570240717a8d
FenrisWolf <albertomg_94@hotmail.com>
2022-06-08 12:04:50 -0500

    Se agrego el boton de modificar y eliminar en permisos

--- layouts/MasterPage/content/_permisos.php
+++ layouts/MasterPage/content/_permisos.php
@@ -11,6 +11,7 @@
                                     <tr class="bg-black text-white uppercase text-sm leading-normal">
                                         <th class="py-3 text-left all">Id</th>
                                         <th class="py-3 text-left all">Permiso</th>
+                                        <th class="py-3 text-left all"></th>
                                     </tr>
                                 </thead>
                             </table>
