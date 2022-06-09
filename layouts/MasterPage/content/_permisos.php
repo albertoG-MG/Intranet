a1fb990aba21b4d710f905f1dc97446e0ff31bf3
Fenriswolf <albertomg_94@hotmail.com>
2022-06-09 00:02:28 -0500

    Se agrego el modal default just in case

--- layouts/MasterPage/content/_permisos.php
+++ layouts/MasterPage/content/_permisos.php
@@ -37,14 +37,14 @@
                                 <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Nombre del permiso</label>
                                 <div class="group flex">
                                     <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
-                                    <input class="w-full -ml-10 pl-10 py-2 px-3 rounded-lg border-2 border-indigo-600 mt-1 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:border-transparent" type="text" id="permiso" name="permiso" placeholder="Input 1">
+                                    <input class="w-full -ml-10 pl-10 py-2 px-3 rounded-lg border-2 border-indigo-600 mt-1 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:border-transparent" type="text" id="crearpermiso" name="crearpermiso" placeholder="Input 1">
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="modal-actions bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                    <button id="guardar-permiso" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-md px-4 py-2 bg-indigo-700 font-medium text-white hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-200 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Guardar</button>
+                    <button id="crear-permiso" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-md px-4 py-2 bg-indigo-700 font-medium text-white hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-200 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Crear</button>
                     <button id="close-modal" type="button" class="w-full inline-flex justify-center rounded-md border border-gray-300 shadow-md px-4 py-2 mt-3 bg-white font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-200 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cerrar</button>
                 </div>
             </form>
