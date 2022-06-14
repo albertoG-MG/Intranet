7b734d32648adf67fbf24726495369ca317e2409
FenrisWolf <albertomg_94@hotmail.com>
2022-06-14 10:20:42 -0500

    Se agrego el div faltante en el script de los forms

--- layouts/MasterPage/scripts/_permisosscript.php
+++ layouts/MasterPage/scripts/_permisosscript.php
@@ -77,11 +77,12 @@ document.addEventListener("DOMContentLoaded", function() {
             "<h3 class='text-lg font-medium text-gray-900'>Crear permiso</h3>"+
             "</div>"+
             "<div class='modal-content text-center w-full mt-3 sm:mt-0 sm:mt-0 sm:ml-4 sm:text-left'>"+
-            "<div class='grid grid-cols-1 mt-5 mx-6 px-3'>"+
-                "<label class='uppercase md:text-sm text-xs text-gray-500 text-light font-semibold'>Nombre del permiso</label>"+
-                "<div class='group flex'>"+
-                    "<div class='w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center'><i class='mdi mdi-lock-outline text-gray-400 text-lg'></i></div>"+
-                    "<input class='w-full -ml-10 pl-10 py-2 px-3 rounded-lg border-2 border-indigo-600 mt-1 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:border-transparent' type='text' id='crearpermiso' name='crearpermiso' placeholder='Input 1'>"+
+                "<div class='grid grid-cols-1 mt-5 mx-6 px-3'>"+
+                    "<label class='uppercase md:text-sm text-xs text-gray-500 text-light font-semibold'>Nombre del permiso</label>"+
+                    "<div class='group flex'>"+
+                        "<div class='w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center'><i class='mdi mdi-lock-outline text-gray-400 text-lg'></i></div>"+
+                        "<input class='w-full -ml-10 pl-10 py-2 px-3 rounded-lg border-2 border-indigo-600 mt-1 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:border-transparent' type='text' id='crearpermiso' name='crearpermiso' placeholder='Input 1'>"+
+                    "</div>"+
                 "</div>"+
             "</div>");
         $('.modal-actions').html(
@@ -152,11 +153,12 @@ document.addEventListener("DOMContentLoaded", function() {
             "<h3 class='text-lg font-medium text-gray-900'>Editar permiso</h3>"+
             "</div>"+
             "<div class='modal-content text-center w-full mt-3 sm:mt-0 sm:mt-0 sm:ml-4 sm:text-left'>"+
-            "<div class='grid grid-cols-1 mt-5 mx-6 px-3'>"+
-                "<label class='uppercase md:text-sm text-xs text-gray-500 text-light font-semibold'>Editar el permiso</label>"+
-                "<div class='group flex'>"+
-                    "<div class='w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center'><i class='mdi mdi-lock-outline text-gray-400 text-lg'></i></div>"+
-                    "<input class='w-full -ml-10 pl-10 py-2 px-3 rounded-lg border-2 border-indigo-600 mt-1 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:border-transparent' type='text' id='editpermiso' name='editpermiso' value='"+data[1]+"'>"+
+                "<div class='grid grid-cols-1 mt-5 mx-6 px-3'>"+
+                    "<label class='uppercase md:text-sm text-xs text-gray-500 text-light font-semibold'>Editar el permiso</label>"+
+                    "<div class='group flex'>"+
+                        "<div class='w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center'><i class='mdi mdi-lock-outline text-gray-400 text-lg'></i></div>"+
+                        "<input class='w-full -ml-10 pl-10 py-2 px-3 rounded-lg border-2 border-indigo-600 mt-1 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:border-transparent' type='text' id='editpermiso' name='editpermiso' value='"+data[1]+"'>"+
+                    "</div>"+
                 "</div>"+
             "</div>");
         $('.modal-actions').html(
