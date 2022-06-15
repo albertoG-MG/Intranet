fcb9b5345b72034ed37ea6db7cd3d24cbdc759fe
FenrisWolf <albertomg_94@hotmail.com>
2022-06-15 10:18:45 -0500

    Se agrego crear rol a la intranet

---
+++ layouts/crear_rol.php
@@ -0,0 +1,9 @@
+<?php
+$title = 'Crear Rol';
+$sidebar = '/components/sidebar.php';
+$navbar = '/components/navbar.php';
+$content = '/content/_crear_rol.php';
+$codigophp = '/phpcode/_crear_rolphp.php';
+$scripts = '/scripts/_crear_rolscript.php';
+include_once __DIR__ . '/MasterPage/masterpage.php';
+?>
