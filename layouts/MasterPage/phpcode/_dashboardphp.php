5f8ca44754f2411bc0b785ce9d0eb593939529c0
FenrisWolf <albertomg_94@hotmail.com>
2022-06-15 17:34:35 -0500

    Se arreglo un bug cuando el usuario no tiene rol

--- layouts/MasterPage/phpcode/_dashboardphp.php
+++ layouts/MasterPage/phpcode/_dashboardphp.php
@@ -9,7 +9,7 @@
         die();
     }
     
-    $perfil = $object->_db->prepare("SELECT usuarios.nombre AS nombre, usuarios.apellido_pat AS apellido_pat, usuarios.apellido_mat AS apellido_mat, roles.nombre AS rolnom FROM usuarios INNER JOIN roles ON usuarios.roles_id=roles.id WHERE usuarios.id=:sessionid");
+    $perfil = $object->_db->prepare("SELECT usuarios.nombre AS nombre, usuarios.apellido_pat AS apellido_pat, usuarios.apellido_mat AS apellido_mat, roles.nombre AS rolnom FROM usuarios LEFT JOIN roles ON usuarios.roles_id=roles.id WHERE usuarios.id=:sessionid");
     $perfil->bindParam("sessionid", $_SESSION['id'], PDO::PARAM_INT);
     $perfil->execute();
     $profile = $perfil -> fetch(PDO::FETCH_OBJ);
