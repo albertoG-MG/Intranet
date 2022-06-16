14588acddfabacbdff1bcfe42161fc39128257a4
FenrisWolf <albertomg_94@hotmail.com>
2022-06-16 11:55:21 -0500

    Se necesita estar logueado para acceder a crear rol

--- layouts/MasterPage/phpcode/_crear_rolphp.php
+++ layouts/MasterPage/phpcode/_crear_rolphp.php
@@ -1,3 +1,12 @@
 <?php
 include_once __DIR__ . "/../../../classes/permissions.php";
+include_once __DIR__ . "/../../../config/conexion.php";
+$object = new connection_database();
+
+session_start();
+if ($_SESSION['loggedin'] != true) {
+    $_SESSION['redirectURL'] = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('Location: Login.php');
+    die();
+}
 ?>
