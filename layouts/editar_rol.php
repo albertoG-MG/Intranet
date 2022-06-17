4cd38539c72095adb716df7447d77762ae69a9f9
FenrisWolf <albertomg_94@hotmail.com>
2022-06-17 09:13:14 -0500

    Se agrego el contenido a editar rol

---
+++ layouts/editar_rol.php
@@ -0,0 +1,9 @@
+<?php
+$title = 'Editar Rol';
+$sidebar = '/components/sidebar.php';
+$navbar = '/components/navbar.php';
+$content = '/content/_editar_rol.php';
+$codigophp = '/phpcode/_editar_rolphp.php';
+$scripts = '/scripts/_editar_rolscript.php';
+include_once __DIR__ . '/MasterPage/masterpage.php';
+?>
