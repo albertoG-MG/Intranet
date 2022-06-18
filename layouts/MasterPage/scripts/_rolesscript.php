9e2b4b43e95e555f419220625265cbbecde9b365
FenrisWolf <albertomg_94@hotmail.com>
2022-06-18 11:34:35 -0500

    Se termino el ajax para eliminar

--- layouts/MasterPage/scripts/_rolesscript.php
+++ layouts/MasterPage/scripts/_rolesscript.php
@@ -81,7 +81,45 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     $('#datatable').on('click', 'tr .Eliminar', function(){
-        alert("hola");
+        var table = $('#datatable').DataTable();
+        var tr = $(this).closest('tr');
+        var row = table.row(tr);
+        var data = row.data();
+        Swal.fire({
+            title: '¿Estas seguro?',
+            text: "No podras recuperar la información!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí!',
+            cancelButtonText: 'cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'éxito',
+                    text: 'La fila ha sido eliminada!'
+                }).then(function() {
+                    var eliminarid = data[0];
+                    var fd = new FormData();
+                    fd.append('id', eliminarid);
+                    $.ajax({
+                        url: "../ajax/eliminar/tabla_roles/eliminarroles.php",
+                        type: "post",
+                        data: fd,
+                        processData: false,
+                        contentType: false,
+                        success: function(result) {
+                            table
+                                .row($(this).parents('tr'))
+                                .remove()
+                                .draw();
+                        }
+                    });
+                });
+            }
+        })
     });
 
 
