d14736d2010a0bff4321c3a3e6ec9e3182be617b
FenrisWolf <albertomg_94@hotmail.com>
2022-06-20 15:40:46 -0500

    Se termino el departamentos script

---
+++ layouts/MasterPage/content/_departamentos.php
@@ -0,0 +1,23 @@
+<div class="container mx-auto px-6 py-8">
+    <h3 class="text-gray-700 text-3xl font-medium">Departamentos</h3>
+    <div class="mt-4">
+        <div class="flex flex-col mt-8">
+            <div class="overflow-x-auto">
+                <div class="min-w-screen bg-transparent flex items-center justify-center bg-gray-100 font-sans overflow-hidden">
+                    <div class="w-full">
+                        <div class="bg-white p-3 shadow-md rounded">
+                            <table class="w-full" id="datatable" style="display:none;">
+                                <thead>
+                                    <tr class="bg-black text-white uppercase text-sm leading-normal">
+                                        <th class="py-3 text-left all">Departamento</th>
+                                        <th class="py-3 text-center min-tablet"></th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
