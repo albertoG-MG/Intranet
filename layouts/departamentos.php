da177e1340787d1f237b8b4ec9fdd3ccd439c0c5
FenrisWolf <albertomg_94@hotmail.com>
2022-06-20 15:24:26 -0500

    Se agrego el template para departamentos

---
+++ layouts/departamentos.php
@@ -0,0 +1,9 @@
+<?php
+$title = 'Departamentos';
+$sidebar = '/components/sidebar.php';
+$navbar = '/components/navbar.php';
+$content = '/content/_departamentos.php';
+$codigophp = '/phpcode/_departamentosphp.php';
+$scripts = '/scripts/_departamentosscript.php';
+include_once __DIR__ . '/MasterPage/masterpage.php';
+?>
