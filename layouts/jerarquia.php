223c5fa83a1a6f90e172ee2e6abb87812ee029d0
FenrisWolf <albertomg_94@hotmail.com>
2022-06-21 10:18:04 -0500

    Se vincularon los archivos para la jerarquia

---
+++ layouts/jerarquia.php
@@ -0,0 +1,9 @@
+<?php
+$title = 'Jerarquía';
+$sidebar = '/components/sidebar.php';
+$navbar = '/components/navbar.php';
+$content = '/content/_jerarquia.php';
+$codigophp = '/phpcode/_jerarquiaphp.php';
+$scripts = '/scripts/_jerarquiascript.php';
+include_once __DIR__ . '/MasterPage/masterpage.php';
+?>
