3b5ce3f164b38e5066f88bf0205f64317dc50bf4
FenrisWolf <albertomg_94@hotmail.com>
2022-06-22 14:59:02 -0500

    Se hizo una pequena modificion en la tabla usuarios

--- layouts/MasterPage/content/_users.php
+++ layouts/MasterPage/content/_users.php
@@ -19,7 +19,7 @@
                                     </svg>
                                 </span>
 
-                                <a href="users.php" class="flex items-center text-gray-600 -px-2 hover:underline">
+                                <a href="users.php" class="flex items-center text-blue-600 -px-2 hover:underline">
                                     <svg class="w-6 h-6 mx-2" viewBox="0 0 24 24">
                                         <path fill="currentColor" d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z" />
                                     </svg>
