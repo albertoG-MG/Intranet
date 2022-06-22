2030f1d1a8737c75dd08407ca0856f28b518bdd2
FenrisWolf <albertomg_94@hotmail.com>
2022-06-22 14:30:53 -0500

    Se agrego el menu de navegacion en la tabla de usuarios

--- layouts/MasterPage/content/_users.php
+++ layouts/MasterPage/content/_users.php
@@ -5,7 +5,31 @@
             <div class="overflow-x-auto">
                 <div class="min-w-screen bg-transparent flex items-center justify-center bg-gray-100 font-sans overflow-hidden">
                     <div class="w-full">
-                        <div class="bg-white p-3 shadow-md rounded">
+                        <div class="bg-gray-50 shadow-md rounded-t">
+                            <div class="container flex flex-col sm:flex-row items-center px-6 py-4 mx-auto overflow-y-auto whitespace-nowrap">
+                                <a href="dashboard.php" class="text-gray-600">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                                        <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+                                    </svg>
+                                </a>
+
+                                <span class="mx-5 rotate-90 sm:rotate-0 text-gray-500">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                                        <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                                    </svg>
+                                </span>
+
+                                <a href="users.php" class="flex items-center text-gray-600 -px-2 hover:underline">
+                                    <svg class="w-6 h-6 mx-2" viewBox="0 0 24 24">
+                                        <path fill="currentColor" d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z" />
+                                    </svg>
+
+                                    <span class="mx-2">Usuarios</span>
+                                </a>
+
+                            </div>
+                        </div>
+                        <div class="bg-white p-3 shadow-md rounded-b">
                             <table class="w-full" id="datatable" style="display:none;">
                                 <thead>
                                     <tr class="bg-black text-white uppercase text-sm leading-normal">
