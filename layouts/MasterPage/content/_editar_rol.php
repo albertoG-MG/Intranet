4e4dedda2f690538d05e5b1e6b553c035bd804c8
FenrisWolf <albertomg_94@hotmail.com>
2022-06-23 16:12:42 -0500

    Lo mismo pero para editar roles

--- layouts/MasterPage/content/_editar_rol.php
+++ layouts/MasterPage/content/_editar_rol.php
@@ -27,7 +27,7 @@
               <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Escriba el nombre del rol</label>
               <div class="group flex">
                 <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
-                <input class="w-full -ml-10 pl-10 py-2 px-3 rounded-lg border-2 border-indigo-600 mt-1 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:border-transparent" type="text" id="rol" name="rol" value="<?php echo "{$row->nombre}"; ?>" placeholder="Input 1">
+                <input class="w-full -ml-10 pl-10 py-2 px-3 rounded-lg border border-gray-200 focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" type="text" id="rol" name="rol" value="<?php echo "{$row->nombre}"; ?>" placeholder="Input 1">
               </div>
             </div>
 
