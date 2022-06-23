504b6d046391465055f1b19199dcf128ca41911b
FenrisWolf <albertomg_94@hotmail.com>
2022-06-23 15:18:16 -0500

    Se cambio el color de la foto en editar usuarios

--- layouts/MasterPage/content/_editar_usuario.php
+++ layouts/MasterPage/content/_editar_usuario.php
@@ -140,11 +140,11 @@
             <div class="grid grid-cols-1 mt-5 mx-7">
               <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold mb-1">Subir foto</label>
               <div class='flex items-center justify-center w-full'>
-                <label class='flex flex-col border-4 border-dashed w-full h-32 hover:bg-gray-100 hover:border-indigo-800 group'>
+                <label class='flex flex-col border-4 border-dashed w-full h-32 hover:bg-gray-100 hover:border-black group'>
                   <div class='flex flex-col items-center justify-center pt-7'>
                     <div id="svg">
                       <?php if($row->foto == null && $row->nombre_foto == null){ ?>
-                        <svg class="w-10 h-10 text-indigo-600 group-hover:text-indigo-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <svg class="w-10 h-10 text-gray-400 group-hover:text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
                         </svg>
                       <?php }else{ ?>
@@ -152,7 +152,7 @@
                       <?php } ?>
                     </div>
                     <img id="preview" class="hidden" />
-                    <p id="archivo" class='lowercase text-sm text-gray-400 group-hover:text-indigo-800 pt-1 tracking-wider'><?php if($row->foto ==null){ echo "Selecciona una fotografía"; }else{ echo $row->nombre_foto;} ?></p>
+                    <p id="archivo" class='lowercase text-sm text-gray-400 group-hover:text-black pt-1 tracking-wider'><?php if($row->foto ==null){ echo "Selecciona una fotografía"; }else{ echo $row->nombre_foto;} ?></p>
                   </div>
                   <input type='file' id="foto" name="foto" class="hidden" />
                 </label>
