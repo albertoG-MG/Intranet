c52a35cc7ad4e01e39aefd1fd2be9d0e35a67336
FenrisWolf <albertomg_94@hotmail.com>
2022-06-23 17:19:41 -0500

    Se agrego el menu en crear roles

--- layouts/MasterPage/content/_crear_rol.php
+++ layouts/MasterPage/content/_crear_rol.php
@@ -8,6 +8,46 @@
     <div class="flex flex-col mt-8">
       <div class="flex bg-gray-200 items-center justify-center mb-32">
         <div class="grid bg-white rounded-lg shadow-xl w-11/12 md:w-9/12">
+          <div class="bg-gray-50 rounded-t">
+            <div class="container flex flex-col sm:flex-row items-center px-6 py-4 mx-auto overflow-y-auto whitespace-nowrap">
+              <a href="dashboard.php" class="text-gray-600">
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+                </svg>
+              </a>
+
+              <span class="mx-5 rotate-90 sm:rotate-0 text-gray-500">
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                </svg>
+              </span>
+
+              <a href="roles.php" class="flex items-center text-gray-600 -px-2 hover:underline">
+                <svg class="w-6 h-6 mx-2" viewBox="0 0 24 24">
+                  <path fill="currentColor" d="M6 8C6 5.79 7.79 4 10 4S14 5.79 14 8 12.21 12 10 12 6 10.21 6 8M9.14 19.75L8.85 19L9.14 18.25C9.84 16.5 11.08 15.14 12.61 14.22C11.79 14.08 10.92 14 10 14C5.58 14 2 15.79 2 18V20H9.27C9.23 19.91 9.18 19.83 9.14 19.75M17 18C16.44 18 16 18.44 16 19S16.44 20 17 20 18 19.56 18 19 17.56 18 17 18M23 19C22.06 21.34 19.73 23 17 23S11.94 21.34 11 19C11.94 16.66 14.27 15 17 15S22.06 16.66 23 19M19.5 19C19.5 17.62 18.38 16.5 17 16.5S14.5 17.62 14.5 19 15.62 21.5 17 21.5 19.5 20.38 19.5 19Z" />
+                </svg>
+
+                <span class="mx-2">Roles</span>
+              </a>
+
+              <span class="mx-5 rotate-90 sm:rotate-0 text-gray-500">
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                </svg>
+              </span>
+
+              <a href="crear_rol.php" class="flex items-center text-blue-600 -px-2 hover:underline">
+                <svg class="w-6 h-6 mx-2" viewBox="0 0 24 24">
+                  <path fill="currentColor" d="M15,14C12.33,14 7,15.33 7,18V20H23V18C23,15.33 17.67,14 15,14M6,10V7H4V10H1V12H4V15H6V12H9V10M15,12A4,4 0 0,0 19,8A4,4 0 0,0 15,4A4,4 0 0,0 11,8A4,4 0 0,0 15,12Z" />
+                </svg>
+
+                <span class="mx-2">Crear Roles</span>
+              </a>
+
+            </div>
+          </div>
+
+
           <div class="flex justify-center py-4">
             <div class="flex bg-white rounded-full md:p-4 p-2 border-2 border-black">
               <svg style="width:48px;" viewBox="0 0 24 24">
@@ -34,15 +74,15 @@
             <div class="grid grid-cols-1 mt-5 mx-7">
               <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Permisos</label>
               <div id="permissionarray" class="flex flex-col gap-3 sm:flex-wrap sm:flex-row">
-              <?php 
-              $permisos = permissions::FetchPermisos();
-              foreach ($permisos as $row){
-                echo "<div class='flex gap-3 items-center' style='flex: 1 0 21%'>";
-                echo "<input type='checkbox' value='".$row->id."'>" .$row->nombre. "</>";
-                echo "</div>";
-              }
-              ?>
-            </div>
+                <?php
+                $permisos = permissions::FetchPermisos();
+                foreach ($permisos as $row) {
+                  echo "<div class='flex gap-3 items-center' style='flex: 1 0 21%'>";
+                  echo "<input type='checkbox' value='" . $row->id . "'>" . $row->nombre . "</>";
+                  echo "</div>";
+                }
+                ?>
+              </div>
             </div>
 
             <div class='flex items-center justify-center  md:gap-8 gap-4 pt-5 pb-5'>
