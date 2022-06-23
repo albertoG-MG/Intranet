7b284aff445b3c4eda287f6b06e0ea15f479ff46
FenrisWolf <albertomg_94@hotmail.com>
2022-06-23 16:20:17 -0500

    Se agrego la imagen para los roles

--- layouts/MasterPage/content/_crear_rol.php
+++ layouts/MasterPage/content/_crear_rol.php
@@ -9,10 +9,10 @@
       <div class="flex bg-gray-200 items-center justify-center mb-32">
         <div class="grid bg-white rounded-lg shadow-xl w-11/12 md:w-9/12">
           <div class="flex justify-center py-4">
-            <div class="flex bg-purple-200 rounded-full md:p-4 p-2 border-2 border-purple-300">
-              <svg class="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path>
-              </svg>
+            <div class="flex bg-white rounded-full md:p-4 p-2 border-2 border-black">
+            <svg style="width:48px;" viewBox="0 0 24 24">
+              <path fill="currentColor" d="M6 8C6 5.79 7.79 4 10 4S14 5.79 14 8 12.21 12 10 12 6 10.21 6 8M12 18.2C12 17.24 12.5 16.34 13.2 15.74V15.5C13.2 15.11 13.27 14.74 13.38 14.38C12.35 14.14 11.21 14 10 14C5.58 14 2 15.79 2 18V20H12V18.2M22 18.3V21.8C22 22.4 21.4 23 20.7 23H15.2C14.6 23 14 22.4 14 21.7V18.2C14 17.6 14.6 17 15.2 17V15.5C15.2 14.1 16.6 13 18 13C19.4 13 20.8 14.1 20.8 15.5V17C21.4 17 22 17.6 22 18.3M19.5 15.5C19.5 14.7 18.8 14.2 18 14.2C17.2 14.2 16.5 14.7 16.5 15.5V17H19.5V15.5Z" />
+            </svg>
             </div>
           </div>
 
