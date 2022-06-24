a5700c1c22f3f6a6896c3e9781db1a55dd8304d2
FenrisWolf <albertomg_94@hotmail.com>
2022-06-24 17:06:38 -0500

    Se agrego el script que abre el menu de navegacion

---
+++ layouts/MasterPage/scripts/_crear_expedientescript.php
@@ -0,0 +1,7 @@
+<script>
+    <?php
+    if(basename($_SERVER['PHP_SELF']) == 'crear_expediente.php'){?>
+        var dropdown = document.getElementById('catalogos');
+        dropdown.classList.remove("hidden");
+    <?php } ?>
+</script>
