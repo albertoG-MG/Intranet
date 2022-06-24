68f790f8cb9cccc1a1eed08c1232dc7554ca9d6f
FenrisWolf <albertomg_94@hotmail.com>
2022-06-24 16:03:03 -0500

    Se agrego el codigo para la masterpage de expedientes

---
+++ layouts/expedientes.php
@@ -0,0 +1,9 @@
+<?php
+$title = 'Expedientes';
+$sidebar = '/components/sidebar.php';
+$navbar = '/components/navbar.php';
+$content = '/content/_expedientes.php';
+$codigophp = '/phpcode/_expedientesphp.php';
+$scripts = '/scripts/_expedientesscript.php';
+include_once __DIR__ . '/MasterPage/masterpage.php';
+?>
