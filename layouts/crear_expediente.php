818b3708ec3d041ee93ce8d00b5b0b071826b009
FenrisWolf <albertomg_94@hotmail.com>
2022-06-24 16:56:41 -0500

    Se agrego la informacion a crear expedientes para la masterpage

---
+++ layouts/crear_expediente.php
@@ -0,0 +1,9 @@
+<?php
+$title = 'Crear Expedientes';
+$sidebar = '/components/sidebar.php';
+$navbar = '/components/navbar.php';
+$content = '/content/_crear_expediente.php';
+$codigophp = '/phpcode/_crear_expedientephp.php';
+$scripts = '/scripts/_crear_expedientescript.php';
+include_once __DIR__ . '/MasterPage/masterpage.php';
+?>
