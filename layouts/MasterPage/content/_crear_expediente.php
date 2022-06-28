42385c926f92f8ef5a854c4b634367afba0891bf
FenrisWolf <albertomg_94@hotmail.com>
2022-06-28 11:13:25 -0500

    Se deshabilito el departamento

--- layouts/MasterPage/content/_crear_expediente.php
+++ layouts/MasterPage/content/_crear_expediente.php
@@ -86,7 +86,7 @@
                                         <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Departamento</label>
                                         <div class="group flex">
                                             <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-account-box-multiple text-gray-400 text-lg"></i></div>
-                                            <input class="w-full -ml-10 pl-10 py-2 px-3 rounded-lg border border-gray-200 focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" type="text" id="departamento" name="departamento" placeholder="Input 2">
+                                            <input class="w-full -ml-10 pl-10 py-2 px-3 bg-gray-200 rounded-lg border border-gray-200 focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" type="text" id="departamento" name="departamento" placeholder="Input 2" readonly>
                                         </div>
                                     </div>
                                     <div class="grid grid-cols-1">
