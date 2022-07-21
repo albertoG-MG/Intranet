6bb7dde0d0add74572c25000a348671219cf08b8
FenrisWolf <albertomg_94@hotmail.com>
2022-07-21 12:15:12 -0500

    Se arreglo el numero de las referencias laborales

--- layouts/MasterPage/scripts/_crear_expedientescript.php
+++ layouts/MasterPage/scripts/_crear_expedientescript.php
@@ -174,7 +174,7 @@
                input.name = "infa_rnombre" + i;
                input.classList.add('w-full', '-ml-10', 'pl-10', 'py-2', 'px-3', 'rounded-lg', 'border', 'border-gray-200', 'focus:outline-none', 'focus:ring-2', 'focus:ring-black', 'focus:border-transparent', 'required');
                input.setAttribute("data-msg", "Este campo es requerido");
-               input.setAttribute("placeholder", "Nombre " +i); 
+               input.setAttribute("placeholder", "Nombre " +(i+1)); 
                grupo.appendChild(input);
                div2.appendChild(document.createTextNode("Parentesco " + (i+1) + " *"));
                var grupo2 = document.createElement("div");
@@ -191,7 +191,7 @@
                input2.name = "infa_rparentesco" + i;
                input2.classList.add('w-full', '-ml-10', 'pl-10', 'py-2', 'px-3', 'rounded-lg', 'border', 'border-gray-200', 'focus:outline-none', 'focus:ring-2', 'focus:ring-black', 'focus:border-transparent', 'required');
                input2.setAttribute("data-msg", "Este campo es requerido");
-               input2.setAttribute("placeholder", "Parentesco " +i);
+               input2.setAttribute("placeholder", "Parentesco " +(i+1));
                grupo2.appendChild(input2);
                div3.appendChild(document.createTextNode("Teléfono " + (i+1) + " *"));
                var grupo3 = document.createElement("div");
@@ -208,7 +208,7 @@
                input3.name = "infa_rtelefono" + i;
                input3.classList.add('w-full', '-ml-10', 'pl-10', 'py-2', 'px-3', 'rounded-lg', 'border', 'border-gray-200', 'focus:outline-none', 'focus:ring-2', 'focus:ring-black', 'focus:border-transparent', 'required');
                input3.setAttribute("data-msg", "Este campo es requerido");
-               input3.setAttribute("placeholder", "Teléfono " +i);
+               input3.setAttribute("placeholder", "Teléfono " +(i+1));
                grupo3.appendChild(input3);
             }
          }
