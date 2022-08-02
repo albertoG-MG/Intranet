68f4970a60023905f275eefb338e6c9b1075a404
FenrisWolf <albertomg_94@hotmail.com>
2022-08-02 11:10:17 -0500

    Se agrego el metodo de nacimiento

--- classes/expedientes.php
+++ classes/expedientes.php
@@ -239,7 +239,23 @@ class expedientes {
     }
 
     public static function Nacimiento($exp_id, $p_nacimiento){
-
+        $crudnacimiento = new crud();
+        $papeleria = 3;
+        $filename = $p_nacimiento["name"];
+        $location = "../src/pdfs_uploaded/".$filename;
+        if(move_uploaded_file($p_nacimiento['tmp_name'],$location)){
+            $pdf_base64 = base64_encode(file_get_contents('../src/pdfs_uploaded/'.$filename));
+            $pdf = 'data:application/pdf;base64,'.$pdf_base64;
+            date_default_timezone_set("America/Monterrey");
+            $fecha_subida = date('y-m-d h:i:s');
+            $crudnacimiento -> store('papeleria_empleado', ['expediente_id' => $exp_id, 'tipo_archivo' => $papeleria, 'nombre_archivo' => $filename, 'archivo' => $pdf, 'fecha_subida' => $fecha_subida]);
+            $files = glob('../src/pdfs_uploaded/*.pdf'); // get all file names
+            foreach($files as $file){ // iterate files
+                if(is_file($file)) {
+                    unlink($file); // delete file
+                }
+            }
+        }
     }
 
     public static function Curp($exp_id, $p_curp){
