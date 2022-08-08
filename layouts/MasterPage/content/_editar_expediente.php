a98a067b935ff2d1cbd0a5e026f42319c84a27d2
FenrisWolf <albertomg_94@hotmail.com>
2022-08-08 09:51:02 -0500

    Se quito el 0 en las referencias laborales y bancarias

--- layouts/MasterPage/content/_editar_expediente.php
+++ layouts/MasterPage/content/_editar_expediente.php
@@ -317,7 +317,7 @@
                                     <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Número de referencias laborales</label>
                                     <div class="group flex">
                                         <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-numeric text-gray-400 text-lg"></i></div>
-                                        <input class="w-full -ml-10 pl-10 py-2 px-3 rounded-lg border border-gray-200 focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" type="text" id="reflab" name="reflab" oninput="AgregarReferencias()" maxlength="1" value="<?php echo "{$cont_referencias}"; ?>" placeholder="Input 16">
+                                        <input class="w-full -ml-10 pl-10 py-2 px-3 rounded-lg border border-gray-200 focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" type="text" id="reflab" name="reflab" oninput="AgregarReferencias()" maxlength="1" value="<?php if($cont_referencias != 0){ echo "{$cont_referencias}";} ?>" placeholder="Input 16">
                                     </div>
                                 </div>
                                 <div id="referencias">
@@ -426,7 +426,7 @@
                                     <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Número de referencias bancarias</label>
                                     <div class="group flex">
                                         <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-numeric text-gray-400 text-lg"></i></div>
-                                        <input class="w-full -ml-10 pl-10 py-2 px-3 rounded-lg border border-gray-200 focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" type="text" id="refban" name="refban" oninput="AgregarBanco()" value="<?php echo "{$cont_datos}"; ?>" maxlength="1" value="" placeholder="Input 26">
+                                        <input class="w-full -ml-10 pl-10 py-2 px-3 rounded-lg border border-gray-200 focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" type="text" id="refban" name="refban" oninput="AgregarBanco()" value="<?php if($cont_datos != 0){ echo "{$cont_datos}";} ?>" maxlength="1" value="" placeholder="Input 26">
                                     </div>
                                 </div>
                                 <div id="ref">
