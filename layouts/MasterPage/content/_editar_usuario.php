de9db4c3a3a050896b6592a4b986859c41f74ab5
FenrisWolf <albertomg_94@hotmail.com>
2022-08-12 14:46:54 -0500

    Se agregaron las validaciones en editar usuario

--- layouts/MasterPage/content/_editar_usuario.php
+++ layouts/MasterPage/content/_editar_usuario.php
@@ -187,18 +187,14 @@
             <div class="grid grid-cols-1 mt-5 mx-7">
               <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold mb-1">Subir foto</label>
               <div class='flex items-center justify-center w-full'>
-                <label class='flex flex-col border-4 border-dashed w-full h-32 hover:bg-gray-100 hover:border-black group'>
+                <label class='flex flex-col border-4 border-dashed w-full hover:bg-gray-100 hover:border-black group'>
                   <div class='flex flex-col items-center justify-center pt-7'>
-                    <div id="svg">
-                      <?php if ($row->foto == null && $row->nombre_foto == null) { ?>
-                        <svg class="w-10 h-10 text-gray-400 group-hover:text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
-                        </svg>
-                      <?php } else { ?>
-                        <img class="w-10 h-10" <?php echo "src={$row->foto}"; ?> />
-                      <?php } ?>
+                    <div id="svg" <?php if($row->foto != null && $row->nombre_foto != null){ echo "class='hidden'";  } ?>>
+                    <svg class="w-10 h-10 text-gray-400 group-hover:text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
+                    </svg>
                     </div>
-                    <img id="preview" class="hidden" />
+                    <img id="preview" <?php if($row->foto != null && $row->nombre_foto != null){ echo "class='w-10 h-10' src='".$row->foto."'"; }else { echo "class='hidden'"; } ?>/>
                     <p id="archivo" class='lowercase text-sm text-center text-gray-400 group-hover:text-black pt-1 tracking-wider'><?php if ($row->foto == null) {
                                                                                                                           echo "Selecciona una fotografía";
                                                                                                                         } else {
