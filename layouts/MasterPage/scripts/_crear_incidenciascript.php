feb2a2848770a870e2b2ae952886a6aa0be72cdb
FenrisWolf <albertomg_94@hotmail.com>
2022-08-15 15:45:04 -0500

    Se agrego la validacion de fechas en crear incidencia

--- layouts/MasterPage/scripts/_crear_incidenciascript.php
+++ layouts/MasterPage/scripts/_crear_incidenciascript.php
@@ -132,4 +132,11 @@
 			}
 		});
 	});
+	
+	function rdfechafin() {
+		$('#fechafin').prop('disabled', false);
+		$('#fechafin').removeClass('bg-gray-200');
+		var fecha_inicio = $('#fechainicio').val();
+		$('#fechafin').attr("min", fecha_inicio);
+	}
 </script>
