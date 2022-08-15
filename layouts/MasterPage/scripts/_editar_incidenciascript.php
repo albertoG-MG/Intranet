382bbee5e81f19072219c082a5eff0f6eb22c66d
FenrisWolf <albertomg_94@hotmail.com>
2022-08-15 17:37:07 -0500

    Se agrego el cambio de mensaje de min en la fecha fin

--- layouts/MasterPage/scripts/_editar_incidenciascript.php
+++ layouts/MasterPage/scripts/_editar_incidenciascript.php
@@ -5,6 +5,7 @@
 		$('#fechafin').removeClass('bg-gray-200');
 		var fecha_inicio = $('#fechainicio').val();
 		$('#fechafin').attr("min", fecha_inicio);
+		$('#fechafin').attr("data-msg-min", 'Por favor, ingrese una fecha mayor o igual que ' +fecha_inicio);
 	}
 
 </script>
