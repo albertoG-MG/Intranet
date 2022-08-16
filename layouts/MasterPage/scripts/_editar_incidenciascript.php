fe238858af17f3cf1f91d8cb6517cdcc5385b362
FenrisWolf <albertomg_94@hotmail.com>
2022-08-16 13:57:40 -0500

    Por fin se arreglaron las fechas en editar incidencia

--- layouts/MasterPage/scripts/_editar_incidenciascript.php
+++ layouts/MasterPage/scripts/_editar_incidenciascript.php
@@ -109,9 +109,7 @@
 		}
 	});
     
-    function rdfechafin() {
-		$('#fechafin').prop('disabled', false);
-		$('#fechafin').removeClass('bg-gray-200');
+	function rdfechafin() {
 		var fecha_inicio = $('#fechainicio').val();
 		$('#fechafin').attr("min", fecha_inicio);
 		$('#fechafin').attr("data-msg-min", 'Por favor, ingrese una fecha mayor o igual que ' +fecha_inicio);
