347a1e53a92f56c3dcd94f855f960fd442ee2997
FenrisWolf <albertomg_94@hotmail.com>
2022-08-22 10:08:24 -0500

    Se agrego el icono de ver en la tabla de usuarios

--- layouts/MasterPage/scripts/_usersscript.php
+++ layouts/MasterPage/scripts/_usersscript.php
@@ -102,6 +102,12 @@
                         return (
                             "<div class='py-3 text-left'>" +
                             "<div class='flex item-center justify-center'>" +
+                            "<div class='w-4 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer Ver'>" +
+                            "<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>"+
+                            "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M15 12a3 3 0 11-6 0 3 3 0 016 0z' />"+
+                            "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z' />"+
+                            "</svg>"+
+                            "</div>" +
                             "<div class='w-4 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer Editar'>" +
                             "<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>" +
                             "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z'></path>" +
@@ -122,6 +128,21 @@
     $(document).ready(function() {
         $('.dataTables_filter input[type="search"]').
         attr('placeholder', 'Buscar...').attr('class', 'search w-full rounded-lg text-gray-600 font-medium')
+    });
+
+    $('#datatable').on('click', 'tr .Ver', function () {
+        var table = $('#datatable').DataTable();
+        var rowSelector;
+        var li = $(this).closest('li');
+        if ( li.length ) {
+            rowSelector = table.cell( li ).index().row;
+        }
+        else {
+            rowSelector =  $(this).closest('tr');
+        }
+        var row = table.row(rowSelector);
+        var data = row.data();
+        window.location.href = "ver_usuario.php?idUser="+data[0]+""; 
     });
 
     $('#datatable').on('click', 'tr .Editar', function () {
