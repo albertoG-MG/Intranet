5f10f9e0cfa4c8fc07e98218a08b88702822169d
FenrisWolf <albertomg_94@hotmail.com>
2022-08-22 09:28:27 -0500

    Se agrego el contenido a ver usuarios

--- layouts/MasterPage/components/sidebar.php
+++ layouts/MasterPage/components/sidebar.php
@@ -35,7 +35,7 @@
 
     <ul id="catalogos" class="hidden py-2 space-y-2">
         <li>
-            <?php if(basename($_SERVER['PHP_SELF']) == 'users.php' || basename($_SERVER['PHP_SELF']) == 'crear_usuario.php' || basename($_SERVER['PHP_SELF']) == 'editar_usuario.php'){?>
+            <?php if(basename($_SERVER['PHP_SELF']) == 'users.php' || basename($_SERVER['PHP_SELF']) == 'crear_usuario.php' || basename($_SERVER['PHP_SELF']) == 'editar_usuario.php' || basename($_SERVER['PHP_SELF']) == 'ver_usuario.php'){?>
                 <a href="users.php" class="flex items-center p-2 pl-11 w-full transition duration-75 bg-gray-700 bg-opacity-25 text-gray-100">
             <?php }else{ ?>
             <a href="users.php" class="flex items-center p-2 pl-11 w-full transition duration-75 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
