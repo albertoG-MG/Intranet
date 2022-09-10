1f43bbae2c712507a74df79b32d07d610b5d4829
FenrisWolf <albertomg_94@hotmail.com>
2022-09-10 12:31:38 -0500

    Se agrego el boton de pendientes y su ajax

---
+++ config/incidencias/incidencia_pendiente.php
@@ -0,0 +1,26 @@
+<?php
+require 'conexion.php';
+$object = new connection_database();
+
+/*$idrol = $_POST["rol"];*/
+$id = $_POST["sessionid"];
+$pendiente = 4;
+
+/*$rolnombre = $object -> _db -> prepare("SELECT roles.nombre FROM usuarios INNER JOIN roles ON usuarios.roles_id=roles.id WHERE roles_id= :rolid");
+$rolnombre -> execute(array(":rolid" => $idrol));
+$fetch_rolnombre = $rolnombre -> fetch(PDO::FETCH_OBJ);
+
+if($fetch_rolnombre -> nombre == "Superadministrador" || $fetch_rolnombre -> nombre == "Administrador"){
+    $consulta = "SELECT incidencias.id as incidenciaid, usuarios.nombre as nombre, usuarios.apellido_pat as apellido_pat, usuarios.apellido_mat as apellido_mat, incidencias.tipo_incidencia as tipo_incidencia, incidencias.fecha_inicio as fecha_inicio, incidencias.fecha_fin as fecha_fin, estatus_incidencia.nombre as estatus_nombre FROM incidencias INNER JOIN usuarios ON incidencias.users_id = usuarios.id INNER JOIN estatus_incidencia ON incidencias.estatus_id=estatus_incidencia.id";
+    $resultado = $object->_db->prepare($consulta);
+}else{*/
+$consulta = "SELECT incidencias.id as incidenciaid, usuarios.nombre as nombre, usuarios.apellido_pat as apellido_pat, usuarios.apellido_mat as apellido_mat, incidencias.tipo_incidencia as tipo_incidencia, incidencias.fecha_inicio as fecha_inicio, incidencias.fecha_fin as fecha_fin, estatus_incidencia.nombre as estatus_nombre FROM incidencias INNER JOIN usuarios ON incidencias.users_id = usuarios.id INNER JOIN estatus_incidencia ON incidencias.estatus_id=estatus_incidencia.id WHERE usuarios.id = :iduser AND estatus_incidencia.id=:estatus";
+$resultado = $object->_db->prepare($consulta);
+$resultado -> bindParam('iduser', $id, PDO::PARAM_INT);
+$resultado -> bindParam('estatus', $pendiente, PDO::PARAM_INT);
+/*}*/
+$resultado->execute();
+$data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+
+print json_encode($data, JSON_UNESCAPED_UNICODE);
+?>
--- layouts/MasterPage/scripts/_incidenciasscript.php
+++ layouts/MasterPage/scripts/_incidenciasscript.php
@@ -41,6 +41,36 @@
 								})
 							}
                         },
+                        {
+                            text: "<i class='mdi mdi-clock text-white font-semibold text-lg'></i> Pendientes",
+                            attr: {
+                                'id': 'incidencias_pendientes',
+                                'style': 'background:rgb(79 70 229 / var(--tw-border-opacity));'
+                            },
+                            className: 'bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg shadow-xl font-medium text-white',
+							action: function ( e, dt, node, config ) {
+								$.ajax({
+									url: "../config/incidencias/incidencia_pendiente.php",
+									method: 'POST',
+									data:{
+										"rol": <?php echo $_SESSION["rol"]; ?>,
+										"sessionid": <?php echo $_SESSION["id"]; ?>
+
+									},
+									success: function(response) {
+										 var table = $('#datatable').DataTable();
+										 table.clear().draw();
+										 const obj = JSON.parse(response);
+										 table.rows.add(obj).draw();
+										 buttonlist = 0; 
+										 table.column().cells().invalidate().render();
+									
+									}, error: function(response) {
+										console.log(response);
+									}
+								})
+							}
+                        },
 						{
                             text: "<i class='mdi mdi-beaker-plus text-white font-semibold text-lg'></i> Crear Incidencia",
                             attr: {
