dff4d707c2df7c1f61292d7f581e69a0408190db
FenrisWolf <albertomg_94@hotmail.com>
2022-09-13 09:56:07 -0500

    Se agrego el content en layout, se coloreo el menu y se agrego la validacion del login

---
+++ layouts/solicitud_incidencia.php
@@ -0,0 +1,9 @@
+<?php
+$title = 'Ver solicitudes de incidencia';
+$sidebar = '/components/sidebar.php';
+$navbar = '/components/navbar.php';
+$content = '/content/_solicitud_incidencia.php';
+$codigophp = '/phpcode/_solicitud_incidenciaphp.php';
+$scripts = '/scripts/_solicitud_incidenciascript.php';
+include_once __DIR__ . '/MasterPage/masterpage.php';
+?>
