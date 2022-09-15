36b83e6636f2cb158edb94ef041863e57804de92
FenrisWolf <albertomg_94@hotmail.com>
2022-09-15 10:27:28 -0500

    Se agrego el boton de rechazados en solicitudes

--- layouts/MasterPage/scripts/_solicitud_incidenciascript.php
+++ layouts/MasterPage/scripts/_solicitud_incidenciascript.php
@@ -83,6 +83,42 @@
 								})
 							}
                         },
+
+                        {
+                            text: "<i class='mdi mdi-close-thick text-white font-semibold text-lg'></i> Solicitudes Rechazadas",
+                            attr: {
+                                'id': 'sol_rechazadas',
+                                'style': 'background:rgb(79 70 229 / var(--tw-border-opacity));'
+                            },
+                            className: 'bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg shadow-xl font-medium text-white',
+							action: function ( e, dt, node, config ) {
+								$.ajax({
+									url: "../config/solicitud_incidencia/solicitud_rechazada.php",
+									method: 'POST',
+									data:{
+										"rol": <?php echo $_SESSION["rol"]; ?>,
+										"sessionid": <?php echo $_SESSION["id"]; ?>
+
+									},
+									success: function(response) {
+										 var table = $('#datatable').DataTable();
+										 table.clear().draw();
+										 const obj = JSON.parse(response);
+										 table.rows.add(obj).draw();
+										 evaluation_buttons=1;
+										 goce_sueldo=1;
+                                         var status = table.column(5);
+                                         status.visible(true);
+                                         var evaluation = table.column(7);
+                                         evaluation.visible(false);
+										 table.column().cells().invalidate().render();
+									
+									}, error: function(response) {
+										console.log(response);
+									}
+								})
+							}
+                        }
 					],
             "ajax":{
                 "url": "../config/ajax_solicitud_incidencia.php",
