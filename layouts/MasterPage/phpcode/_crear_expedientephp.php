873d40168c67b78e620893a0eb346f53e888ad9f
FenrisWolf <albertomg_94@hotmail.com>
2022-09-19 14:15:37 -0500

    Se cambiaron las mayusculas por minusculas en todos los php

--- layouts/MasterPage/phpcode/_crear_expedientephp.php
+++ layouts/MasterPage/phpcode/_crear_expedientephp.php
@@ -7,7 +7,7 @@
 
     if ($_SESSION['loggedin'] != true) {
         $_SESSION['redirectURL'] = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-        header('Location: Login.php');
+        header('Location: login.php');
         die();
     }
 
--- layouts/MasterPage/phpcode/_crear_usuariophp.php
+++ layouts/MasterPage/phpcode/_crear_usuariophp.php
@@ -7,7 +7,7 @@
     session_start();
     if ($_SESSION['loggedin'] != true) {
         $_SESSION['redirectURL'] = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-        header('Location: Login.php');
+        header('Location: login.php');
         die();
     }
 ?>
