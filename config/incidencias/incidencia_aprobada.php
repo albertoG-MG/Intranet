0f46f2f4ea733ec2859f8ac233728986ec875087
FenrisWolf <albertomg_94@hotmail.com>
2022-09-24 10:38:18 -0500

    Lo mismo que el commit anterior pero con aprobados

--- config/incidencias/incidencia_aprobada.php
+++ config/incidencias/incidencia_aprobada.php
@@ -1,37 +1,21 @@
 <?php
-require '../conexion.php';
-$object = new connection_database();
+	require '../conexion.php';
+	$object = new connection_database();
 
-/*$idrol = $_POST["rol"];*/
-$id = $_POST["sessionid"];
-$aprobado = 1;
+	$id = $_POST["sessionid"];
+	$aprobado = 1;
 
-/*$rolnombre = $object -> _db -> prepare("SELECT roles.nombre FROM usuarios INNER JOIN roles ON usuarios.roles_id=roles.id WHERE roles_id= :rolid");
-$rolnombre -> execute(array(":rolid" => $idrol));
-$fetch_rolnombre = $rolnombre -> fetch(PDO::FETCH_OBJ);*/
 
-$select_rol_dep = $object -> _db -> prepare("SELECT roles.nombre as rolnom, departamentos.departamento as depanom FROM usuarios INNER JOIN roles ON roles.id=usuarios.roles_id LEFT JOIN departamentos ON departamentos.id=usuarios.departamento_id WHERE usuarios.id=:userid");
-$select_rol_dep -> execute(array(":userid" => $id));
-$fetch_rol_dep = $select_rol_dep -> fetch(PDO::FETCH_OBJ);
+	$select_rol_dep = $object -> _db -> prepare("SELECT roles.nombre as rolnom, departamentos.departamento as depanom FROM usuarios INNER JOIN roles ON roles.id=usuarios.roles_id LEFT JOIN departamentos ON departamentos.id=usuarios.departamento_id WHERE usuarios.id=:userid");
+	$select_rol_dep -> execute(array(":userid" => $id));
+	$fetch_rol_dep = $select_rol_dep -> fetch(PDO::FETCH_OBJ);
 
-/*if($fetch_rolnombre -> nombre == "Superadministrador" || $fetch_rolnombre -> nombre == "Administrador"){
-    $consulta = "SELECT incidencias.id as incidenciaid, usuarios.nombre as nombre, usuarios.apellido_pat as apellido_pat, usuarios.apellido_mat as apellido_mat, incidencias.tipo_incidencia as tipo_incidencia, incidencias.fecha_inicio as fecha_inicio, incidencias.fecha_fin as fecha_fin, estatus_incidencia.nombre as estatus_nombre FROM incidencias INNER JOIN usuarios ON incidencias.users_id = usuarios.id INNER JOIN estatus_incidencia ON incidencias.estatus_id=estatus_incidencia.id";
-    $resultado = $object->_db->prepare($consulta);
-}else{*/
-if($fetch_rol_dep -> rolnom != "Superadministrador" && $fetch_rol_dep -> rolnom != "Administrador" && $fetch_rol_dep -> rolnom != "Director general"){
-    $consulta = "SELECT incidencias.id as incidenciaid, usuarios.nombre as nombre, usuarios.apellido_pat as apellido_pat, usuarios.apellido_mat as apellido_mat, incidencias.tipo_incidencia as tipo_incidencia, incidencias.fecha_inicio as fecha_inicio, incidencias.fecha_fin as fecha_fin, estatus_incidencia.nombre as estatus_nombre, accion_incidencias.goce_de_sueldo as sueldo FROM incidencias INNER JOIN usuarios ON incidencias.users_id = usuarios.id INNER JOIN estatus_incidencia ON incidencias.estatus_id=estatus_incidencia.id LEFT JOIN accion_incidencias ON accion_incidencias.incidencias_id = incidencias.id WHERE usuarios.id=:iduser AND incidencias.estatus_id =:estatus";
-    $resultado = $object->_db->prepare($consulta);
-    $resultado -> bindParam('iduser', $id, PDO::PARAM_INT);
-    $resultado -> bindParam('estatus', $aprobado, PDO::PARAM_INT);
-/*}*/
-$resultado->execute();
-}else{
-    $consulta = "SELECT incidencias.id as incidenciaid, usuarios.nombre as nombre, usuarios.apellido_pat as apellido_pat, usuarios.apellido_mat as apellido_mat, incidencias.tipo_incidencia as tipo_incidencia, incidencias.fecha_inicio as fecha_inicio, incidencias.fecha_fin as fecha_fin, estatus_incidencia.nombre as estatus_nombre, accion_incidencias.goce_de_sueldo as sueldo FROM incidencias INNER JOIN usuarios ON incidencias.users_id = usuarios.id INNER JOIN estatus_incidencia ON incidencias.estatus_id=estatus_incidencia.id LEFT JOIN accion_incidencias ON accion_incidencias.incidencias_id = incidencias.id WHERE incidencias.estatus_id =:estatus";
-    $resultado = $object->_db->prepare($consulta);
-    $resultado -> bindParam('estatus', $aprobado, PDO::PARAM_INT);
-    $resultado->execute();
-}
-$data=$resultado->fetchAll(PDO::FETCH_ASSOC);
-
-print json_encode($data, JSON_UNESCAPED_UNICODE);
+	if($fetch_rol_dep -> rolnom == "Superadministrador" || $fetch_rol_dep -> rolnom == "Administrador" || $fetch_rol_dep -> rolnom == "Director general"){
+		$consulta = "SELECT incidencias.id as incidenciaid, usuarios.nombre as nombre, usuarios.apellido_pat as apellido_pat, usuarios.apellido_mat as apellido_mat, incidencias.tipo_incidencia as tipo_incidencia, incidencias.fecha_inicio as fecha_inicio, incidencias.fecha_fin as fecha_fin, estatus_incidencia.nombre as estatus_nombre, accion_incidencias.goce_de_sueldo as sueldo FROM incidencias INNER JOIN usuarios ON incidencias.users_id = usuarios.id INNER JOIN estatus_incidencia ON incidencias.estatus_id=estatus_incidencia.id LEFT JOIN accion_incidencias ON accion_incidencias.incidencias_id = incidencias.id WHERE incidencias.estatus_id =:estatus";
+		$resultado = $object->_db->prepare($consulta);
+		$resultado -> bindParam('estatus', $aprobado, PDO::PARAM_INT);
+		$resultado->execute();
+		$data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+		print json_encode($data, JSON_UNESCAPED_UNICODE);
+	}
 ?>
--- layouts/MasterPage/scripts/_incidenciasscript.php
+++ layouts/MasterPage/scripts/_incidenciasscript.php
@@ -86,9 +86,51 @@
 								})
 							}
                         },
-                        <?php } ?>
+                        <?php 
+                        }  
+                        if($count_jerarquia > 0){
+                            if($fetch_jerarquia != null){
+                        ?>
                         {
-                            text: "<i class='mdi mdi-check-bold text-white font-semibold text-lg'></i> Aprobados",
+							text: "<i class='mdi mdi-check-bold text-white font-semibold text-lg'></i>Mis Aprobadas",
+							attr: {
+								'id': 'mis_incidencias_aprobadas',
+								'style': 'background:rgb(79 70 229 / var(--tw-border-opacity));'
+							},
+							className: 'w-full bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg shadow-xl font-medium text-white',
+							action: function ( e, dt, node, config ) {
+								$.ajax({
+									url: "../config/incidencias/mi_incidencia_aprobada.php",
+									method: 'POST',
+									data:{
+										"rol": <?php echo $_SESSION["rol"]; ?>,
+										"sessionid": <?php echo $_SESSION["id"]; ?>
+
+									},
+									success: function(response) {
+										 var table = $('#datatable').DataTable();
+										 table.clear().draw();
+										 const obj = JSON.parse(response);
+										 table.rows.add(obj).draw();
+										 buttonlist = 1;
+										 var sueldo = table.column(6);
+										 sueldo.visible(true); 
+										 table.column().cells().invalidate().render();
+										 table.columns.adjust().responsive.recalc();
+									
+									}, error: function(response) {
+										console.log(response);
+									}
+								})
+							}
+						},
+                        <?php 
+							}
+						}
+                        if(Roles::FetchSessionRol($_SESSION["rol"]) == "Superadministrador" || Roles::FetchSessionRol($_SESSION["rol"]) == "Administrador" || Roles::FetchSessionRol($_SESSION["rol"]) == "Director general"){
+						?>
+                        {
+                            text: "<i class='mdi mdi-check-bold text-white font-semibold text-lg'></i> Aprobadas",
                             attr: {
                                 'id': 'incidencias_aprobadas',
                                 'style': 'background:rgb(79 70 229 / var(--tw-border-opacity));'
@@ -120,6 +162,7 @@
 								})
 							}
                         },
+                        <?php } ?>
                         {
                             text: "<i class='mdi mdi-close-thick text-white font-semibold text-lg'></i> Rechazadas",
                             attr: {
