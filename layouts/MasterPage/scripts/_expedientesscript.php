9010c9f28737d1c3191e2f13b4c5518b7d817230
FenrisWolf <albertomg_94@hotmail.com>
2022-09-30 11:59:51 -0500

    Se agregaron los permisos en la tabla expedientes

--- layouts/MasterPage/scripts/_expedientesscript.php
+++ layouts/MasterPage/scripts/_expedientesscript.php
@@ -11,6 +11,7 @@
             },
             dom: '<"top"fB>rt<"bottom"ip><"clear">',
             buttons: [
+                <?php if (Permissions::CheckPermissions($_SESSION["id"], "Crear expediente") == "true" || Roles::FetchSessionRol($_SESSION["rol"]) == "Superadministrador" || Roles::FetchSessionRol($_SESSION["rol"]) == "Administrador") { ?>
                         {
                             text: "<i class='mdi mdi-account-box text-white font-semibold text-lg'></i> Crear Expediente",
                             attr: {
@@ -22,6 +23,7 @@
                                 window.location.href = "crear_expediente.php";
                             }
                         }
+                <?php } ?>
                     ],
             "processing": true,
             "serverSide": true,
@@ -69,22 +71,28 @@
                         return (
                             "<div class='py-3 text-left'>" +
                             "<div class='flex item-center justify-center'>" +
+                            <?php if (Permissions::CheckPermissions($_SESSION["id"], "Ver expediente") == "true" || Roles::FetchSessionRol($_SESSION["rol"]) == "Superadministrador" || Roles::FetchSessionRol($_SESSION["rol"]) == "Administrador") { ?>
                             "<div class='w-4 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer Ver'>" +
                             "<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>"+
                             "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M15 12a3 3 0 11-6 0 3 3 0 016 0z' />"+
                             "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z' />"+
                             "</svg>"+
                             "</div>" +
+                            <?php } ?>
+                            <?php if (Permissions::CheckPermissions($_SESSION["id"], "Editar expediente") == "true" || Roles::FetchSessionRol($_SESSION["rol"]) == "Superadministrador" || Roles::FetchSessionRol($_SESSION["rol"]) == "Administrador") { ?>
                             "<div class='w-4 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer Editar'>" +
                             "<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>" +
                             "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z'></path>" +
                             "</svg>" +
                             "</div>" +
+                            <?php } ?>
+                            <?php if (Permissions::CheckPermissions($_SESSION["id"], "Eliminar expediente") == "true" || Roles::FetchSessionRol($_SESSION["rol"]) == "Superadministrador" || Roles::FetchSessionRol($_SESSION["rol"]) == "Administrador") { ?>
                             "<div class='w-4 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer Eliminar'>" +
                             "<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>" +
                             "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16'></path>" +
                             "</svg>" +
                             "</div>" +
+                            <?php } ?>
                             "</div>" +
                             "</div>");
                     }
@@ -98,37 +106,41 @@
     $('.dataTables_filter input[type="search"]').
     attr('placeholder', 'Buscar...').attr('class', 'search w-full rounded-lg text-gray-600 font-medium');
 
-    $('#datatable').on('click', 'tr .Ver', function () {
-        var table = $('#datatable').DataTable();
-        var rowSelector;
-        var li = $(this).closest('li');
-        if ( li.length ) {
-            rowSelector = table.cell( li ).index().row;
-        }
-        else {
-            rowSelector =  $(this).closest('tr');
-        }
-        var row = table.row(rowSelector);
-        var data = row.data();
-        window.location.href = "ver_expediente.php?idExpediente="+data[0]+""; 
-    });
+    <?php if (Permissions::CheckPermissions($_SESSION["id"], "Ver expediente") == "true" || Roles::FetchSessionRol($_SESSION["rol"]) == "Superadministrador" || Roles::FetchSessionRol($_SESSION["rol"]) == "Administrador") { ?>
+        $('#datatable').on('click', 'tr .Ver', function () {
+            var table = $('#datatable').DataTable();
+            var rowSelector;
+            var li = $(this).closest('li');
+            if ( li.length ) {
+                rowSelector = table.cell( li ).index().row;
+            }
+            else {
+                rowSelector =  $(this).closest('tr');
+            }
+            var row = table.row(rowSelector);
+            var data = row.data();
+            window.location.href = "ver_expediente.php?idExpediente="+data[0]+""; 
+        });
+    <?php } ?>
 
-    $('#datatable').on('click', 'tr .Editar', function () {
-        var table = $('#datatable').DataTable();
-        var rowSelector;
-        var li = $(this).closest('li');
-        if ( li.length ) {
-            rowSelector = table.cell( li ).index().row;
-        }
-        else {
-            rowSelector =  $(this).closest('tr');
-        }
-        var row = table.row(rowSelector);
-        var data = row.data();
-        window.location.href = "editar_expediente.php?idExpediente="+data[0]+""; 
-    });
+    <?php if (Permissions::CheckPermissions($_SESSION["id"], "Editar expediente") == "true" || Roles::FetchSessionRol($_SESSION["rol"]) == "Superadministrador" || Roles::FetchSessionRol($_SESSION["rol"]) == "Administrador") { ?>
+        $('#datatable').on('click', 'tr .Editar', function () {
+            var table = $('#datatable').DataTable();
+            var rowSelector;
+            var li = $(this).closest('li');
+            if ( li.length ) {
+                rowSelector = table.cell( li ).index().row;
+            }
+            else {
+                rowSelector =  $(this).closest('tr');
+            }
+            var row = table.row(rowSelector);
+            var data = row.data();
+            window.location.href = "editar_expediente.php?idExpediente="+data[0]+""; 
+        });
+    <?php } ?>
 
-
+    <?php if (Permissions::CheckPermissions($_SESSION["id"], "Eliminar expediente") == "true" || Roles::FetchSessionRol($_SESSION["rol"]) == "Superadministrador" || Roles::FetchSessionRol($_SESSION["rol"]) == "Administrador") { ?>
     $('#datatable').on('click', 'tr .Eliminar', function() {
         var table = $('#datatable').DataTable();
         var rowSelector;
@@ -191,6 +203,7 @@
 	        })
         })
     });
+    <?php } ?>
 
     function check_user_logged(){
         return new Promise((resolve, reject) => {
