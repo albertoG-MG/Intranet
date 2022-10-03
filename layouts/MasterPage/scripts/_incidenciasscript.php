871b4a3b3e7f8db1bfb6f16444309e5330af8618
FenrisWolf <albertomg_94@hotmail.com>
2022-10-03 11:07:11 -0500

    Se agrego lo del cache para la tabla incidencias

--- layouts/MasterPage/scripts/_incidenciasscript.php
+++ layouts/MasterPage/scripts/_incidenciasscript.php
@@ -600,30 +600,63 @@
                 confirmButtonText: 'Sí!',
                 cancelButtonText: 'cancelar'
             }).then((result) => {
-                if (result.isConfirmed) {
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'éxito',
-                        text: 'La fila ha sido eliminada!'
-                    }).then(function() {
-                        var eliminarid = data["incidenciaid"];
-                        var fd = new FormData();
-                        fd.append('id', eliminarid);
-                        $.ajax({
-                            url: "../ajax/eliminar/tabla_incidencias/eliminarincidencia.php",
-                            type: "post",
-                            data: fd,
-                            processData: false,
-                            contentType: false,
-                            success: function(result) {
-                                table.ajax.reload();
-                            }
+                check_user_logged().then((response) => {
+		            if(response == "true"){
+                        if (result.isConfirmed) {
+                            Swal.fire({
+                                icon: 'success',
+                                title: 'éxito',
+                                text: 'La fila ha sido eliminada!'
+                            }).then(function() {
+                                var eliminarid = data["incidenciaid"];
+                                var fd = new FormData();
+                                fd.append('id', eliminarid);
+                                $.ajax({
+                                    url: "../ajax/eliminar/tabla_incidencias/eliminarincidencia.php",
+                                    type: "post",
+                                    data: fd,
+                                    processData: false,
+                                    contentType: false,
+                                    success: function(result) {
+                                        table.ajax.reload();
+                                    }
+                                });
+                            });
+                        }
+                    }else{
+                        Swal.fire({
+                            title: "Ocurrió un error",
+                            text: "Su sesión expiró ó limpio el caché del navegador ó cerro sesión, por favor, vuelva a iniciar sesión!",
+                            icon: "error"
+                        }).then(function() {
+                            window.location.href = "login.php";
                         });
-                    });
-                }
+                    }
+                }).catch((error) => {
+		            console.log(error);
+	            })
             })
         });
         <?php } ?>
+
+        function check_user_logged(){
+            return new Promise((resolve, reject) => {
+                $.ajax({
+                    type: "POST",
+                    url: "../ajax/check_user_logged.php",
+                    data:{
+                        pagina: <?php echo "\"http://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}\"";?>
+                    },
+                    success: function (response) {
+                        resolve(response)
+                    },
+                    error: function (error) {
+                        reject(error)
+                    }
+                });
+            })
+        }
+
 	});
 </script>
 
