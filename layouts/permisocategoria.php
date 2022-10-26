3a8c67b8406e879d60d1ef1b747b070848361b88
FenrisWolf <albertomg_94@hotmail.com>
2022-10-26 13:38:01 -0500

    Se agrego la validacion del login y el contenido de el layout de las categorias

---
+++ layouts/permisocategoria.php
@@ -0,0 +1,9 @@
+<?php
+$title = 'Permisos x Categorías';
+$sidebar = '/components/sidebar.php';
+$navbar = '/components/navbar.php';
+$content = '/content/_permisocategoria.php';
+$codigophp = '/phpcode/_permisocategoriaphp.php';
+$scripts = '/scripts/_permisocategoriascript.php';
+include_once __DIR__ . '/MasterPage/masterpage.php';
+?>
