e968f12e1349966ac55dcc7033369d091eb47ef5
FenrisWolf <albertomg_94@hotmail.com>
2022-10-27 16:47:43 -0500

    Se agrego el layout de crear subrol y se modifico el icono de subroles

--- layouts/MasterPage/scripts/_subrolesscript.php
+++ layouts/MasterPage/scripts/_subrolesscript.php
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
         dom: '<"top"fB>rt<"bottom"ip><"clear">',
         buttons: [
             {
-                text: "<i class='mdi account-supervisor-circle text-white font-semibold text-lg'></i>Crear subrol",
+                text: "<i class='mdi mdi-account-supervisor-circle text-white font-semibold text-lg'></i> Crear subrol",
                 attr: {
                     'id': 'subroles',
                     'style': 'background:rgb(79 70 229 / var(--tw-border-opacity));'
