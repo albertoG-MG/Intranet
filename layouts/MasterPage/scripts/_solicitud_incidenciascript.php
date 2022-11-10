883131fea11484b8dd80eb29ec488a94e9e94b36
FenrisWolf <albertomg_94@hotmail.com>
2022-11-10 09:38:49 -0600

    Se agrego la funcionalidad a los botones en solicitudes

--- layouts/MasterPage/scripts/_solicitud_incidenciascript.php
+++ layouts/MasterPage/scripts/_solicitud_incidenciascript.php
@@ -241,16 +241,6 @@
                         "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z' />"+
                         "</svg>"+
                         "</div>" +
-                        "<div class='w-4 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer Editar'>" +
-                        "<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>" +
-                        "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z'></path>" +
-                        "</svg>" +
-                        "</div>" +
-                        "<div class='w-4 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer Eliminar'>" +
-                        "<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>" +
-                        "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16'></path>" +
-                        "</svg>" +
-                        "</div>" +
                         "</div>" +
                         "</div>"
                     );	
@@ -982,15 +972,19 @@
         });
 
         $('#datatable').on('click', 'tr .Ver', function () {
-            alert("Ver");
-        });
-
-        $('#datatable').on('click', 'tr .Editar', function () {
-            alert("Editar");
-        });
-
-        $('#datatable').on('click', 'tr .Eliminar', function () {
-            alert("Eliminar");
+            var table = $('#datatable').DataTable();
+	        var rowSelector;
+	        var li = $(this).closest('li');
+	        if ( li.length ) {
+		        rowSelector = table.cell( li ).index().row;
+	        }
+	        else {
+		        rowSelector =  $(this).closest('tr');
+	        }
+	        var row = table.row(rowSelector);
+	        var data = row.data();
+	
+	        window.location.href = "ver_incidencia.php?idIncidencia="+data['incidenciaid']+"";
         });
 
         function check_user_logged(){
