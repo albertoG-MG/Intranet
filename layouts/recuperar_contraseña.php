a9ade458a86894e564d8fef73a561607f2286ae1
FenrisWolf <albertomg_94@hotmail.com>
2023-01-11 16:09:24 -0600

    Se hicieron algunas modificaciones en recuperar password

--- layouts/recuperar_contraseña.php
+++ layouts/recuperar_contraseña.php
@@ -15,12 +15,12 @@ if(isset($_POST["password"], $_POST["token"], $_POST["password_confirm"])){
 		if($fetch_token_information -> exp_date >= $today_date){
 			$password = $_POST["password"];
 			$password_confirm = $_POST["password_confirm"];
-			if(strlen($password) < 8 ){
+            if(empty($password)){
+			    die(json_encode(array("error", "La contraseña no puede estar vacía")));
+			}else if(strlen($password) < 8 ){
 				die(json_encode(array("error", "La contraseña debe ser 8 dígitos ó más")));
 			}else if(!preg_match("/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%&*])[a-zA-Z0-9!@#$%&*]+$/", $password)){
 			    die(json_encode(array("error", "La contraseña debe contener al menos un número, una letra en mayúscula, una letra en minúscula y un simbolo especial(!@#$%&*) y no se permiten espacios")));
-			}else if(empty($password)){
-			    die(json_encode(array("error", "La contraseña no puede estar vacía")));
 			}else if(empty($password_confirm)){
 				die(json_encode(array("error", "La confirmación de la contraseña no puede estar vacía")));
 			}else if(strlen($password_confirm) < 8 ){
@@ -151,6 +151,19 @@ if($_GET['token'] == null){
                             </button>
                             </div>
                         </div>
+                        <div id="loader-password" class="hidden mt-5">
+							<svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+							  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"></path>
+							  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"></path>
+							</svg>
+							<span>Cargando...</span>
+						</div>
+						<div class="hidden" id="correct-password">
+							<li class="flex items-center">
+							  <svg aria-hidden="true" class="w-5 h-5 mr-1.5 text-green-500 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+							  <p class="text-green-600">Contraseña válida</p>
+							</li>
+						</div>
                     </div>
                     <div x-data="{showen:true}" class='grid grid-cols-1 mt-5 mx-7'>
                         <div x-show="showen">
@@ -173,6 +186,7 @@ if($_GET['token'] == null){
                     <div class="grid grid-cols-1 mx-7">
                         <ul id="passwordHelpBlock" class="text-light text-sm text-gray-500">
                             <li>La contraseña debe contener al menos un número, una letra en mayúscula, una letra en minúscula y un simbolo especial(!@#$%&*) sin espacios, acentos ni ñ.</li>
+                            <li>Nota: Se hara una verificación a la contraseña para evitar el uso de palabras comunes, contraseñas repetidas y evitar su uso por 365 días.</li>
                         </ul>
                     </div>
                     <div id="submit-button">
@@ -212,6 +226,19 @@ if($_GET['token'] == null){
                                             type: "GET",
                                             data: {
                                                 "token": "<?php echo $token; ?>"
+                                            },
+                                            beforeSend: function () {
+                                                $('#loader-password').removeClass('hidden');
+                                                $('#correct-password').addClass('hidden');
+                                            },
+                                            complete: function(data){
+                                                if(data.responseText == "true") {
+                                                    $('#loader-password').delay(3000).queue(function(next){ $(this).addClass('hidden');    next();  });
+                                                    $('#correct-password').delay(3000).queue(function(next){ $(this).removeClass('hidden');    next();  });
+                                                }else{
+                                                    $('#loader-password').addClass('hidden');
+                                                    $('#correct-password').addClass('hidden');
+                                                }
                                             }
                                         }
                                     },
@@ -223,9 +250,9 @@ if($_GET['token'] == null){
                                 },
                                 messages: {
                                     password: {
-                                        required: 'Por favor, ingrese una contraseña',
-                                        minlength: "La contraseña debe de contener 8 caracteres como mínimo",
-                                        password_validation: "La contraseña debe contener al menos un número, una letra en mayúscula, una letra en minúscula y un simbolo especial(!@#$%&*) y no se permiten espacios"
+                                        required:function () {$('#loader-password').addClass('hidden'); $('#correct-password').addClass('hidden'); $("#password").removeData("previousValue"); return "Por favor, ingresa una contraseña"; },
+                                        minlength:function () {$('#loader-password').addClass('hidden'); $('#correct-password').addClass('hidden'); $("#password").removeData("previousValue"); return "La contraseña debe de contener 8 caracteres como mínimo"; },
+                                        password_validation:function () {$('#loader-password').addClass('hidden'); $('#correct-password').addClass('hidden'); $("#password").removeData("previousValue"); return "Contraseña no válida"; }
                                     },
                                     password_confirm:{
                                         required: "La confirmación de la contraseña no puede estar vacía",
