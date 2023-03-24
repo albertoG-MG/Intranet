046b839badd5ff34dffd17f5788e5bf8c1ed826c
FenrisWolf <albertomg_94@hotmail.com>
2023-03-24 10:04:26 -0600

    Se editó los estilos y la funcionalidad de editar expedientes

--- ajax/validacion/expedientes/checkeditemail.php
+++ ajax/validacion/expedientes/checkeditemail.php
@@ -1,16 +1,16 @@
 <?php 
-		include_once __DIR__ . "/../../../config/conexion.php";
-		$object = new connection_database();
+	include_once __DIR__ . "/../../../config/conexion.php";
+	$object = new connection_database();
 
-		$Editarid = $_POST["editarid"];
-		$correo = $_POST["correo_personal"];
-		$get_correo = $object -> _db -> prepare("SELECT * FROM expedientes WHERE correo_personal=:correo AND id!=:idexpediente");
-		$get_correo -> execute(array(':correo' => $correo, ':idexpediente' => $Editarid));
-		$count_query = $get_correo -> rowCount();
-		if($count_query > 0){
-			$output = false;
-		}else{
-			$output = true;
-		}
-		echo json_encode($output);
+	$Editarid = $_POST["editarid"];
+	$correo = $_POST["correo_adicional"];
+	$get_correo = $object -> _db -> prepare("SELECT correo_adicional from expedientes where correo_adicional=:correo1 AND id!=:idexpediente UNION ALL SELECT correo from usuarios where correo=:correo2");
+	$get_correo -> execute(array(':correo1' => $correo, ':idexpediente' => $Editarid, ':correo2' => $correo));
+	$count_query = $get_correo -> rowCount();
+	if($count_query > 0){
+		$output = false;
+	}else{
+		$output = true;
+	}
+	echo json_encode($output);
 ?>
