5266c7a8988bb945f13b3487c249e24e60a0a9d8
Alberto MG <albertomg_94@hotmail.com>
2023-05-17 16:44:07 +0000

    Se agregó en el sidebar el apartado de vacaciones

--- layouts/MasterPage/components/sidebar.php
+++ layouts/MasterPage/components/sidebar.php
@@ -93,4 +93,18 @@
         <span class="mx-3">Incidencias</span>
     </a>
     <?php } ?>
+
+    <?php if (Permissions::CheckPermissions($_SESSION["id"], "Acceso a vacaciones") == "true" || Roles::FetchSessionRol($_SESSION["rol"]) == "Superadministrador" || Roles::FetchSessionRol($_SESSION["rol"]) == "Administrador") { ?>
+	<?php if(basename($_SERVER['PHP_SELF']) == 'vacaciones.php'){?>
+		<a class="flex items-center mt-4 py-2 px-6 bg-gray-700 bg-opacity-25 text-gray-100" href="vacaciones.php">
+	<?php }else{ ?>
+		<a class="flex items-center mt-4 py-2 px-6 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100" href="vacaciones.php">
+	<?php } ?>
+			<svg aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+				<path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path><path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clip-rule="evenodd"></path>
+			</svg>
+
+			<span class="mx-3">Vacaciones</span>
+		</a>
+    <?php } ?>
 </nav>
