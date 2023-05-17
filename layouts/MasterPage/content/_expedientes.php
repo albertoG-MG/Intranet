cf0b80d47a9ac98d1548cbfbccbea78e68784c57
Alberto MG <albertomg_94@hotmail.com>
2023-05-17 17:18:58 +0000

    Se corrigió el icono del breadcrumb de expedientes

--- layouts/MasterPage/content/_expedientes.php
+++ layouts/MasterPage/content/_expedientes.php
@@ -19,7 +19,7 @@
                                     </svg>
                                 </span>
                                 <div class="flex items-center text-gray-400">
-                                    <svg style="w-5 h-5 mr-2" viewBox="0 0 24 24"><path fill="currentColor" d="M6,17C6,15 10,13.9 12,13.9C14,13.9 18,15 18,17V18H6M15,9A3,3 0 0,1 12,12A3,3 0 0,1 9,9A3,3 0 0,1 12,6A3,3 0 0,1 15,9M3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3H5C3.89,3 3,3.9 3,5Z"></path></svg>
+                                    <svg class="w-5 h-5 mr-2" viewBox="0 0 24 24"><path fill="currentColor" d="M6,17C6,15 10,13.9 12,13.9C14,13.9 18,15 18,17V18H6M15,9A3,3 0 0,1 12,12A3,3 0 0,1 9,9A3,3 0 0,1 12,6A3,3 0 0,1 15,9M3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3H5C3.89,3 3,3.9 3,5Z"></path></svg>
                                     <span class="text-sm font-medium">Expedientes</span>
                                 </div>
                             </div>
