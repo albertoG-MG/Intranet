56640a23e21b38b63c819641e693d79d5d3d58ac
Alberto MG <albertomg_94@hotmail.com>
2023-06-01 20:04:02 +0000

    Se agregó el layout de las actas administrativas y cartas compromiso

---
+++ layouts/actas_cartas.php
@@ -0,0 +1,9 @@
+<?php
+    $title = 'Actas y Cartas';
+    $sidebar = '/components/sidebar.php';
+    $navbar = '/components/navbar.php';
+    $content = '/content/_actas_cartas.php';
+    $codigophp = '/phpcode/_actas_cartasphp.php';
+    $scripts = '/scripts/_actas_cartasscript.php';
+    include_once __DIR__ . '/MasterPage/masterpage.php';
+?>
