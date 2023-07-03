56fdf938d90e938c979469ecd0dc656888815188
Alberto MG <albertomg_94@hotmail.com>
2023-07-03 22:00:47 +0000

    Se terminó de programar la vista de la sección solicitar vacaciones y ya envía al class search

--- layouts/MasterPage/content/_vacaciones.php
+++ layouts/MasterPage/content/_vacaciones.php
@@ -83,6 +83,9 @@
                             </svg>
                             <span>Informarción extra</span>
                         </div>
+                        <div class="px-4 py-2 text-sm text-yellow-800 rounded-lg bg-yellow-50" role="alert">
+                            <span class="font-medium">Alerta!</span> Las vacaciones deben disfrutarse antes de la fecha de vencimiento.
+                        </div>
                         <div class="grid grid-cols-1" style="word-break:break-word;">
                             <div class="px-4 py-2 font-semibold">
                                 Correo
@@ -118,33 +121,35 @@
                     </div>
                 </div>
                 <div class="w-full md:w-9/12 h-64">
-                    <div class="bg-white p-3 shadow-md rounded-b-lg md:rounded-lg">
-                        <div class="flex items-center space-x-3 font-semibold text-gray-900 text-xl leading-8">
-                            <svg aria-hidden="true" class="h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
-                                <path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clip-rule="evenodd"></path>
-                            </svg>
-                            <span class="tracking-wide">Solicitar vacaciones</span>
-                        </div>
-                        <div class="grid grid-cols-1 mt-5 mx-7">
-                            <label class="text-[#64748b] font-semibold mb-2">Periodo de las vacaciones a tomar</label>
-                            <div class="group flex">
-                                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
-                                    <svg class="w-5 h-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                        <path fill="currentColor" d="M9,10H7V12H9V10M13,10H11V12H13V10M17,10H15V12H17V10M19,3H18V1H16V3H8V1H6V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3M19,19H5V8H19V19Z"></path>
-                                    </svg>
+                    <form id="Guardar" method="post">
+                        <div class="bg-white p-3 shadow-md rounded-b-lg md:rounded-lg">
+                            <div class="flex items-center space-x-3 font-semibold text-gray-900 text-xl leading-8">
+                                <svg aria-hidden="true" class="h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
+                                    <path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clip-rule="evenodd"></path>
+                                </svg>
+                                <span class="tracking-wide">Solicitar vacaciones</span>
+                            </div>
+                            <div class="grid grid-cols-1 mt-5 mx-7">
+                                <label class="text-[#64748b] font-semibold mb-2">Periodo de las vacaciones a tomar</label>
+                                <div class="group flex">
+                                    <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                                        <svg class="w-5 h-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                            <path fill="currentColor" d="M9,10H7V12H9V10M13,10H11V12H13V10M17,10H15V12H17V10M19,3H18V1H16V3H8V1H6V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3M19,19H5V8H19V19Z"></path>
+                                        </svg>
+                                    </div>
+                                    <input class="w-full -ml-10 pl-10 py-2 h-11 border rounded-md border-[#d1d5db] focus:ring-2 focus:ring-indigo-600" type="text" id="periodo_vacaciones" name="periodo_vacaciones" placeholder="Periodo de las vacaciones a tomar" autocomplete="off" aria-invalid="false">
                                 </div>
-                                <input class="w-full -ml-10 pl-10 py-2 h-11 border rounded-md border-[#d1d5db] focus:ring-2 focus:ring-indigo-600" type="text" id="periodo_vacaciones" name="periodo_vacaciones" placeholder="Periodo de las vacaciones a tomar" autocomplete="off" aria-invalid="false">
+                            </div>
+                            <div class="grid grid-cols-1 mt-5 mx-7 text-center">
+                                <div id="submit-vacaciones">
+                                    <button class="button bg-indigo-600 text-white rounded-md h-11 px-8 py-2 focus:ring-2 focus:outline-none focus:ring-[#4F46E5]/50 hover:bg-indigo-500 active:bg-indigo-700" id="guardar_general" name="guardar_general" type="submit">
+                                        Solicitar vacaciones
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                        <div class="grid grid-cols-1 mt-5 mx-7 text-center">
-                            <div id="submit-vacaciones">
-                                <button class="button bg-indigo-600 text-white rounded-md h-11 px-8 py-2 focus:ring-2 focus:outline-none focus:ring-[#4F46E5]/50 hover:bg-indigo-500 active:bg-indigo-700" id="guardar_general" type="submit">
-                                    Solicitar vacaciones
-                                </button>
-                            </div>
-                        </div>
-                    </div>
+                    </form>
                     <div class="my-4"></div>
                     <div class="bg-white p-3 shadow-md rounded-lg">
                         <div class="flex items-center space-x-3 font-semibold text-gray-900 text-xl leading-8 mb-3">
