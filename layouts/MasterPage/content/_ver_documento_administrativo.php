36994e8d36d2221e266ad21a9084702a69f0f515
Alberto MG <albertomg_94@hotmail.com>
2023-07-10 19:32:48 +0000

    Se agregó una leyenda a los documentos administrativos y se agregaron los datos de acta a carta

--- layouts/MasterPage/content/_ver_documento_administrativo.php
+++ layouts/MasterPage/content/_ver_documento_administrativo.php
@@ -57,27 +57,33 @@
                 <div class="my-3 h-px bg-slate-200"></div>
               </div>
               <?php if($fetch_information -> tipo == "ACTA ADMINISTRATIVA"){ ?>
+                <div class="px-4 py-2 mt-1 mx-7 text-sm text-yellow-800 rounded-lg bg-yellow-50" role="alert">
+	                <span class="font-medium">ACTA AMINISTRATIVA “F.03”</span> Documento exclusivo para uso interno de SNTTCM Integración de Tecnología y Comunicación.
+                </div>
                 <div id="main_acta" class="mt-5 mx-7 p-2" style="border: 5px solid black;">
                   <div id="information" style="word-break:break-word;">
                     <div class="grid grid-cols-1 mt-1 mx-7">
                       <div class="group flex items-center justify-end gap-3">
                         <p>rh@sinttecom.com</p>
                         <svg class="shrink-0 w-5 h-5 text-gray-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                          <path fill="currentColor" d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z" /></svg></span>
+                          <path fill="currentColor" d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z" />
+                        </svg>
                       </div>
                     </div>
                     <div class="grid grid-cols-1 mt-1 mx-7">
                       <div class="group flex items-center justify-end gap-3">
-                        <p>Vista Grande 211. Col. Linda Vista. Gpe N.L. CP 67130</p>
-                        <svg class="shrink-0 w-5 h-5 text-gray-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                          <path fill="currentColor" d="M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z" /></svg>
+                          <p>Vista Grande 211. Col. Linda Vista. Gpe N.L. CP 67130</p>
+                          <svg class="shrink-0 w-5 h-5 text-gray-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z" />
+                          </svg>
                       </div>
                     </div>
                     <div class="grid grid-cols-1 mt-1 mx-7">
                       <div class="group flex items-center justify-end gap-3">
                         <p>(81)1931 3386 (81)2086 8812 (81)2086 8813</p>
                         <svg class="shrink-0 w-5 h-5 text-gray-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                          <path fill="currentColor" d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z" /></svg>
+                          <path fill="currentColor" d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z" />
+                        </svg>
                       </div>
                     </div>
                   </div>
@@ -116,10 +122,34 @@
                   </div>
                 </div>
               <?php }else{ ?>
+                <div class="px-4 py-2 mt-1 mx-7 text-sm text-yellow-800 rounded-lg bg-yellow-50" role="alert">
+                  <span class="font-medium">CARTA COMPROMISO “F.18”</span> Documento exclusivo para uso interno de SNTTCM Integración de Tecnología y Comunicación.
+                </div>
                 <div id="main_carta" class="mt-5 mx-7 p-2" style="border: 5px solid black;">
                   <div id="information_carta" style="word-break:break-word;">
-                    <div class="grid grid-cols-1 mx-7 text-right">
-                      <span class= "font-semibold">Guadalupe Nuevo León, <?php echo $fetch_information -> fecha; ?></span>
+                    <div class="grid grid-cols-1 mt-1 mx-7">
+                      <div class="group flex items-center justify-end gap-3">
+                        <p>rh@sinttecom.com</p>
+                        <svg class="shrink-0 w-5 h-5 text-gray-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                          <path fill="currentColor" d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z" />
+                        </svg>
+                      </div>
+                    </div>
+                    <div class="grid grid-cols-1 mt-1 mx-7">
+                      <div class="group flex items-center justify-end gap-3">
+                        <p>Vista Grande 211. Col. Linda Vista. Gpe N.L. CP 67130</p>
+                        <svg class="shrink-0 w-5 h-5 text-gray-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                          <path fill="currentColor" d="M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z" />
+                        </svg>
+                      </div>
+                    </div>
+                    <div class="grid grid-cols-1 mt-1 mx-7">
+                      <div class="group flex items-center justify-end gap-3">
+                        <p>(81)1931 3386 (81)2086 8812 (81)2086 8813</p>
+                        <svg class="shrink-0 w-5 h-5 text-gray-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                          <path fill="currentColor" d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z" />
+                        </svg>
+                      </div>
                     </div>
                   </div>
                   <div class="grid grid-cols-1 mt-5">
