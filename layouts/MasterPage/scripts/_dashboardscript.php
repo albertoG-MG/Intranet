49bec28806da55f9285f0c80fd3394ee806971e2
Alberto MG <albertomg_94@hotmail.com>
2023-07-24 23:08:43 +0000

    Update file _dashboardscript.php

--- layouts/MasterPage/scripts/_dashboardscript.php
+++ layouts/MasterPage/scripts/_dashboardscript.php
@@ -292,11 +292,19 @@
                                 {
                                     target: [2],
                                     render: function (data, type, row) {
-                                        return(
-                                            "<div class='text-left'>" +
-                                                "<span>" + row["noticias_foto_identificador"] + "</span>" +
-                                            "</div>"
-                                        );
+                                        if(row["noticias_foto_identificador"] === null){
+                                            return(
+                                                "<div>" +
+                                                    "<img class='block lg:m-auto w-10 h-10 shrink-0' src='../src/img/default_news_image.png'>"+
+                                                "</div>"
+                                            );
+                                        }else{
+                                            return(
+                                                "<div>" +
+                                                    "<img class='block lg:m-auto w-10 h-10 shrink-0' src='../src/noticias/"+row['noticias_foto_identificador']+"' onerror='this.onerror=null; this.src=\"../src/img/not_found.jpg\"'>"+
+                                                "</div>"
+                                            );
+                                        }
                                     }
                                 },
                                 {
@@ -333,15 +341,17 @@
                                     target: [8],
                                     render: function (data, type, row) {
                                         return (
-                                            "<div class='py-3 text-left'>" +
-                                            "<div class='flex item-center justify-center data'>" +
-                                            "<div class='w-4 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer Ver'>" +
-                                            "<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>"+
-                                            "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M15 12a3 3 0 11-6 0 3 3 0 016 0z' />"+
-                                            "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z' />"+
-                                            "</svg>"+
-                                            "</div>" +
-                                            "</div>" +
+                                            "<div class='flex item-center justify-start md:justify-center gap-3'>" +
+                                                "<div class='w-4 transform hover:text-purple-500 hover:scale-110 cursor-pointer Editar'>" +
+                                                    "<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>" +
+                                                    "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z'></path>" +
+                                                    "</svg>" +
+                                                "</div>" +
+                                                "<div class='w-4 transform hover:text-purple-500 hover:scale-110 cursor-pointer Eliminar'>" +
+                                                    "<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>" +
+                                                    "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16'></path>" +
+                                                    "</svg>" +
+                                                "</div>" +
                                             "</div>"
                                         );	
                                     }
