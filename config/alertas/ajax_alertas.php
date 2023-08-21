18c3af4e67af61dfe7f9725b7b8fec7b64623595
Alberto MG <albertomg_94@hotmail.com>
2023-08-21 17:24:24 +0000

    Se ordenó la tabla de alertas empezando por el último id

--- config/alertas/ajax_alertas.php
+++ config/alertas/ajax_alertas.php
@@ -8,7 +8,7 @@
     $id = $_POST["sessionid"];
 
     
-	$consulta = "SELECT a.id, concat(u1.nombre,' ',u1.apellido_pat,' ',u1.apellido_mat) AS creada_por, concat(u2.nombre,' ',u2.apellido_pat,' ',u2.apellido_mat) AS modificado_por, a.titulo_alerta, a.descripcion_alerta, a.fecha_creacion_alerta, a.fecha_modificacion, a.filename_alertas, a.alertas_foto_identificador FROM alertas a INNER JOIN usuarios u1 ON u1.id=a.users_id LEFT JOIN usuarios u2 ON u2.id=a.modificado_por;";
+	$consulta = "SELECT a.id, concat(u1.nombre,' ',u1.apellido_pat,' ',u1.apellido_mat) AS creada_por, concat(u2.nombre,' ',u2.apellido_pat,' ',u2.apellido_mat) AS modificado_por, a.titulo_alerta, a.descripcion_alerta, a.fecha_creacion_alerta, a.fecha_modificacion, a.filename_alertas, a.alertas_foto_identificador FROM alertas a INNER JOIN usuarios u1 ON u1.id=a.users_id LEFT JOIN usuarios u2 ON u2.id=a.modificado_por ORDER BY id DESC;";
 	$resultado = $object->_db->prepare($consulta);
 	$resultado -> execute();
 	$data=$resultado->fetchAll(PDO::FETCH_ASSOC);
