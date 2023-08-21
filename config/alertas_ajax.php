4ff3a7a5ccf986358f5221a58a829721f698e613
Alberto MG <albertomg_94@hotmail.com>
2023-08-21 15:55:23 +0000

    Se cambio noticias ajax por alertas ajax en la raíz de config

--- config/alertas_ajax.php
+++ config/alertas_ajax.php
@@ -6,7 +6,7 @@
     $pageno = $_GET["pageNumber"];
     $offset = ($pageno-1) * $no_of_records_per_page;
 
-    $statement = $object -> _db -> prepare("SELECT noticias.*, CONCAT(usuarios.nombre, ' ', usuarios.apellido_pat, ' ', usuarios.apellido_mat) AS nombre FROM noticias INNER JOIN usuarios ON usuarios.id=noticias.users_id ORDER BY id DESC LIMIT $offset, $no_of_records_per_page");
+    $statement = $object -> _db -> prepare("SELECT alertas.*, CONCAT(usuarios.nombre, ' ', usuarios.apellido_pat, ' ', usuarios.apellido_mat) AS nombre FROM alertas INNER JOIN usuarios ON usuarios.id=alertas.users_id ORDER BY id DESC LIMIT $offset, $no_of_records_per_page");
     $statement -> execute();
     $data=$statement->fetchAll(PDO::FETCH_ASSOC);
     print json_encode(['items' => $data]);
