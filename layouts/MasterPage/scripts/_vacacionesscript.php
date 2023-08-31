8f9733218f41d61e6c578dfc041fa9071cf9d7fe
Alberto MG <albertomg_94@hotmail.com>
2023-08-31 21:11:16 +0000

    Se agregó el filtro para buscar vacaciones en específico

--- layouts/MasterPage/scripts/_vacacionesscript.php
+++ layouts/MasterPage/scripts/_vacacionesscript.php
@@ -527,6 +527,36 @@
     //Inicialización de la librería de las fechas
     $('input[name="periodo_vacaciones"]').daterangepicker({ showDropdowns: true, parentEl: "main", locale: { format: 'YYYY/MM/DD' }, applyButtonClasses: "button bg-indigo-600 px-3 py-3 text-white rounded-md focus:ring-2 focus:outline-none focus:ring-[#4F46E5]/50 hover:bg-indigo-500 active:bg-indigo-700", cancelClass: "button bg-white border border-gray-300 text-gray-600 rounded-md outline-none px-3 py-3 focus:ring-2 focus:outline-none focus:ring-[#d1d5db]/50 hover:bg-gray-50 active:bg-gray-100" });
 
+    <?php if(Roles::FetchSessionRol($_SESSION["rol"]) == "Superadministrador" || Roles::FetchSessionRol($_SESSION["rol"]) == "Administrador" || Permissions::CheckPermissions($_SESSION["id"], "Ver todas las vacaciones") == "true"){ ?>
+        $('input[name="periodo_buscar"]').daterangepicker({ showDropdowns: true, parentEl: "main", locale: { format: 'YYYY/MM/DD' }, applyButtonClasses: "button bg-indigo-600 px-3 py-3 text-white rounded-md focus:ring-2 focus:outline-none focus:ring-[#4F46E5]/50 hover:bg-indigo-500 active:bg-indigo-700", cancelClass: "button bg-white border border-gray-300 text-gray-600 rounded-md outline-none px-3 py-3 focus:ring-2 focus:outline-none focus:ring-[#d1d5db]/50 hover:bg-gray-50 active:bg-gray-100" });
+
+        $('#periodo_buscar').on('apply.daterangepicker', function(ev, picker) {
+            var fd = new FormData();
+            var fecha_inicio = picker.startDate.format('YYYY-MM-DD');
+            var fecha_fin = picker.endDate.format('YYYY-MM-DD');
+            fd.append('fecha_inicio', fecha_inicio);
+            fd.append('fecha_fin', fecha_fin);
+            $.ajax({
+                url: '../ajax/filtros/buscar_vacaciones_periodo/buscarperiodo.php',
+                type: 'POST',
+                data: fd,
+                processData: false,
+                contentType: false,
+                success: function(data) {
+                    var table = $('#datatable').DataTable();
+                    table.clear().draw();
+                    const obj = JSON.parse(data);
+                    table.rows.add(obj).draw();
+                    table.column().cells().invalidate().render();
+                    table.columns.adjust().responsive.recalc();
+                },
+                error: function(data) {
+                    $("#ajax-error").text('Fail to send request');
+                }
+            });        
+        });
+    <?php } ?>
+
     if ($('#Guardar').length > 0) {
             $('#Guardar').validate({
                 ignore: [],
