7f2f0b997d475c90f12e032ffcb4bea2790c4666
Alberto MG <albertomg_94@hotmail.com>
2023-08-31 17:08:10 +0000

    Se arregló la responsividad de los tabs en el dashboard

--- layouts/MasterPage/content/_dashboard.php
+++ layouts/MasterPage/content/_dashboard.php
@@ -58,7 +58,7 @@
                 <li role="presentation" class="w-full md:w-max">
                     <button class="w-full group flex items-center space-x-2 rounded-lg px-4 py-2.5 tracking-wide outline-none transition-all hover:bg-slate-100 hover:text-slate-800 focus:bg-slate-100 focus:text-slate-800" id="avisos-tab-profile" data-tabs-target="#avisos" type="button" role="tab" aria-controls="avisos" aria-selected="false">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-slate-400 transition-colors group-hover:text-slate-500 group-focus:text-slate-500" viewBox="0 0 24 24"><path fill="currentColor" d="M12.04,2.5L9.53,5H14.53L12.04,2.5M4,7V20H20V7H4M12,0L17,5V5H20A2,2 0 0,1 22,7V20A2,2 0 0,1 20,22H4A2,2 0 0,1 2,20V7A2,2 0 0,1 4,5H7V5L12,0M7,18V14H12V18H7M14,17V10H18V17H14M6,12V9H11V12H6Z" /></svg>
-                        <span>Avisos informativos</span>
+                        <span>A. informativos</span>
                     </button>
                 </li>
                 <li role="presentation" class="w-full md:w-max">
