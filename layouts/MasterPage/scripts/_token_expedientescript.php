c644234627d517e30958a5cde2d5c7215177d00a
Alberto MG <albertomg_94@hotmail.com>
2023-09-01 15:58:03 +0000

    Se añadió el archivo script de token expediente

---
+++ layouts/MasterPage/scripts/_token_expedientescript.php
@@ -0,0 +1,6 @@
+<script>
+    <?php if(basename($_SERVER['PHP_SELF']) == 'token_expediente.php'){?>
+		var dropdown = document.getElementById('catalogos');
+		dropdown.classList.remove("hidden");
+	<?php } ?>
+</script>
