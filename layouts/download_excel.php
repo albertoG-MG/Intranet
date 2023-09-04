b2c29d7c933081259e7fa666ef35ad4d1d46fcfe
Alberto MG <albertomg_94@hotmail.com>
2023-09-04 20:09:34 +0000

    Se borró el archivo download_excel.php de layouts

--- layouts/download_excel.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $title = 'Descarga de excel';
-    $sidebar = '/components/sidebar.php';
-    $navbar = '/components/navbar.php';
-    $content = '/content/_download_excel.php';
-    $codigophp = '/phpcode/_download_excelphp.php';
-    $scripts = '/scripts/_download_excelscript.php';
-    include_once __DIR__ . '/MasterPage/masterpage.php';
-?>
