e75fe9aba2777001b95f6fbc34c7f0aaf162b045
Priscilactz <priscila.cortez@sinttecom.com>
2023-10-14 12:59:44 -0600

    Se modificaron las validaciones de CURP

--- config/destruirsesion_expediente.php
+++ config/destruirsesion_expediente.php
@@ -1,4 +1,19 @@
 <?php
+// Permitir solicitudes desde cualquier origen
+header("Access-Control-Allow-Origin: *");
+
+// Permitir los métodos de solicitud que deseas permitir
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+
+// Establecer los encabezados permitidos para la solicitud
+header("Access-Control-Allow-Headers: Content-Type");
+
+// Permitir incluir las credenciales en las solicitudes (por ejemplo, cookies)
+header("Access-Control-Allow-Credentials: true");
+
+// Establecer el tipo de contenido de la respuesta como JSON
+header("Content-Type: application/json");
+
 session_start();
 
 if (isset($_SESSION['expediente_id'])) {
--- layouts/MasterPage/scripts/_crear_expedientescript.php
+++ layouts/MasterPage/scripts/_crear_expedientescript.php
@@ -543,7 +543,7 @@
 
         //Función que valida el CURP
         $.validator.addMethod('curp_validation', function (value, element) {
-            return this.optional(element) || /^([A-Z&]|[a-z&]{1})([AEIOU]|[aeiou]{1})([A-Z&]|[a-z&]{1})([A-Z&]|[a-z&]{1})([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])([HM]|[hm]{1})([AS|as|BC|bc|BS|bs|CC|cc|CS|cs|CH|ch|CL|cl|CM|cm|DF|df|DG|dg|GT|gt|GR|gr|HG|hg|JC|jc|MC|mc|MN|mn|MS|ms|NT|nt|NL|nl|OC|oc|PL|pl|QT|qt|QR|qr|SP|sp|SL|sl|SR|sr|TC|tc|TS|ts|TL|tl|VZ|vz|YN|yn|ZS|zs|NE|ne]{2})([^A|a|E|e|I|i|O|o|U|u]{1})([^A|a|E|e|I|i|O|o|U|u]{1})([^A|a|E|e|I|i|O|o|U|u]{1})([0-9]{2})$/g.test(value);
+            return this.optional(element) || /^([A-Z&]|[a-z&]{1})([A-Z&]|[a-z&]{1})([A-Z&]|[a-z&]{1})([A-Z&]|[a-z&]{1})([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])([HM]|[hm]{1})([AS|as|BC|bc|BS|bs|CC|cc|CS|cs|CH|ch|CL|cl|CM|cm|DF|df|DG|dg|GT|gt|GR|gr|HG|hg|JC|jc|MC|mc|MN|mn|MS|ms|NT|nt|NL|nl|OC|oc|PL|pl|QT|qt|QR|qr|SP|sp|SL|sl|SR|sr|TC|tc|TS|ts|TL|tl|VZ|vz|YN|yn|ZS|zs|NE|ne]{2})([^A|a|E|e|I|i|O|o|U|u]{1})([^A|a|E|e|I|i|O|o|U|u]{1})([^A|a|E|e|I|i|O|o|U|u]{1})([0-9A-Z&]{2})$/g.test(value);
         }, 'not a valid curp.');
 
         //Función que valida el RFC
