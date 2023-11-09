baadfc733ed1296a60af6d38199182cdf7ca1147
Alberto MG <albertomg_94@hotmail.com>
2023-11-09 20:19:11 +0000

    Se agregó el layout de mi historial de vacaciones

---
+++ layouts/mi_historial_vacaciones.php
@@ -0,0 +1,9 @@
+<?php
+    $title = 'Mi Historial De Vacaciones';
+    $sidebar = '/components/sidebar.php';
+    $navbar = '/components/navbar.php';
+    $content = '/content/_mi_historial_vacaciones.php';
+    $codigophp = '/phpcode/_mi_historial_vacacionesphp.php';
+    $scripts = '/scripts/_mi_historial_vacacionesscript.php';
+    include_once __DIR__ . '/MasterPage/masterpage.php';
+?>
